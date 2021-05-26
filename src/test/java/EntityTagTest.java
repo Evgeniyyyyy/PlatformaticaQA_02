a426e70a01f2d455de41006e506fafa1a5604f3a
LMalushkina <larisamalushkina@gmail.com>
2021-05-26 14:30:29 -0700

    AT_023_Java-Entity-Tag. New created record is added

---
+++ src/test/java/EntityTagTest.java
@@ -0,0 +1,33 @@
+import base.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.ProjectUtils;
+import utils.TestUtils;
+
+public class EntityTagTest extends BaseTest{
+
+    @Test
+    public void testCreateNewRecord() {
+
+        ProjectUtils.start(getDriver());
+
+        WebElement navItemTag = getDriver().findElement(By.xpath("//p[contains(text(),'Tag')]"));
+        TestUtils.scroll(getDriver(), navItemTag);
+        navItemTag.click();
+
+        getDriver().findElement(By.xpath("//i[contains(text(),'create_new_folder')]")).click();
+        getDriver().findElement(By.xpath("//input[@id='string']")).sendKeys("String");
+        getDriver().findElement(By.xpath("//textarea[@id='text']")).sendKeys("text");
+        getDriver().findElement(By.xpath("//input[@id='int']")).sendKeys("2");
+        getDriver().findElement(By.xpath("//input[@id='decimal']")).sendKeys("2.20");
+        getDriver().findElement(By.xpath("//input[@id='date']")).click();
+        getDriver().findElement(By.xpath("//input[@id='datetime']")).click();
+        getDriver().findElement(By.xpath("//div[contains(text(),'apptester1@tester.test')]")).isDisplayed();
+        getDriver().findElement(By.xpath("//button[@id='pa-entity-form-save-btn']")).click();
+
+        WebElement checkCreatedRecord = getDriver().findElement(By.xpath("//i[@class='fa fa-check-square-o']"));
+        Assert.assertTrue(checkCreatedRecord.isDisplayed());
+    }
+}
