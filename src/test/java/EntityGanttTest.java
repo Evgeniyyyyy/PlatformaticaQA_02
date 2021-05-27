9f401f17acf160dbe045281fa92b7113d99eca45
Daria-Razzhigaeva <82791007+Daria-Razzhigaeva@users.noreply.github.com>
2021-05-27 11:56:30 -0700

    032 | Entity > Gantt: Create new record and save as draft (#227)
    - Created new test testCreateNewRecordAndSaveAsDraft
    - Added method mainActions

--- src/test/java/EntityGanttTest.java
+++ src/test/java/EntityGanttTest.java
@@ -1,10 +1,7 @@
 import base.BaseTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.util.List;
 import static utils.ProjectUtils.start;
 import static utils.TestUtils.scrollClick;
 
@@ -25,17 +22,14 @@ public class EntityGanttTest extends BaseTest {
     By datetimeField = By.id("datetime");
     By testerNameField = By.xpath("//div[contains(text(),'apptester1@tester.test')]");
     By saveButton = By.id("pa-entity-form-save-btn");
+    By draftButton = By.id("pa-entity-form-draft-btn");
+    By listButton = By.xpath("//div[@class='content']//li[2]//a[1]");
+    By checkSquare = By.xpath("//i[@class='fa fa-check-square-o']");
+    By checkPencil = By.xpath("//i[@class='fa fa-pencil']");
 
-    private int checkRecord(WebDriver driver){
-        List<WebElement> rowsCount = driver.findElements(By.className("e-treecolumn-container"));
-        return rowsCount.size();
-    }
-
-    @Test
-    public void testCreateNewRecordAndSave() {
+    private void mainActions(){
         start(getDriver());
         scrollClick(getDriver(), getDriver().findElement(ganttTab));
-
         getDriver().findElement(createNewRecord).click();
         getDriver().findElement(stringField).sendKeys(STRING_INPUT_VALUE);
         getDriver().findElement(textField).sendKeys(TEXT_INPUT_VALUE);
@@ -44,8 +38,27 @@ public class EntityGanttTest extends BaseTest {
         getDriver().findElement(dateField).click();
         getDriver().findElement(datetimeField).click();
         getDriver().findElement(testerNameField).isDisplayed();
-        getDriver().findElement(saveButton).click();
+    }
 
-        Assert.assertEquals(checkRecord(getDriver()), 1);
+    private void listButtonClick(){
+        getDriver().findElement(listButton).click();
+    }
+
+    @Test
+    public void testCreateNewRecordAndSave() {
+
+        mainActions();
+        getDriver().findElement(saveButton).click();
+        listButtonClick();
+        Assert.assertTrue(getDriver().findElement(checkSquare).isDisplayed());
+    }
+
+    @Test
+    public void testCreateNewRecordAndSaveAsDraft(){
+
+        mainActions();
+        getDriver().findElement(draftButton).click();
+        listButtonClick();
+        Assert.assertTrue(getDriver().findElement(checkPencil).isDisplayed());
     }
 }
