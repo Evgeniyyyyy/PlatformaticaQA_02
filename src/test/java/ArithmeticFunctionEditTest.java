57add82371d88b42411cd13c9060641df0bf0979
Andrey Teterin <robinhood.ex@gmail.com>
2021-05-26 23:45:43 -0400

    AT_071_Java | Entity>Arithmetic Function: Edit an existing record

--- src/test/java/ArithmeticFunctionEditTest.java
+++ src/test/java/ArithmeticFunctionEditTest.java
@@ -11,15 +11,23 @@ import utils.TestUtils;
 
 public class ArithmeticFunctionEditTest extends BaseTest {
 
+    public void newData (WebDriver driver, String x, String y) {
+        WebElement f1 = getDriver().findElement(By.xpath("//input[@data-field_name='f1']"));
+        WebElement f2 = getDriver().findElement(By.xpath("//input[@data-field_name='f2']"));
+        f1.clear();
+        f1.sendKeys(x);
+        f2.clear();
+        f2.sendKeys(y);
+        getDriver().findElement(By.xpath("//button[@id='pa-entity-form-save-btn']")).click();
+    }
+
 
     public void createRecord(WebDriver driver) {
         ProjectUtils.start(getDriver());
         WebElement arithmeticButton = getDriver().findElement(By.xpath("//p[text()=' Arithmetic Function ']/parent::a/parent::li"));
         TestUtils.scrollClick(getDriver(), arithmeticButton);
         getDriver().findElement(By.xpath("//i[text()='create_new_folder']")).click();
-        getDriver().findElement(By.xpath("//input[@data-field_name='f1']")).sendKeys("10");
-        getDriver().findElement(By.xpath("//input[@data-field_name='f2']")).sendKeys("20");
-        getDriver().findElement(By.xpath("//button[@id='pa-entity-form-save-btn']")).click();
+        newData(getDriver(), "10", "20");
     }
 
     @Test
@@ -30,7 +38,9 @@ public class ArithmeticFunctionEditTest extends BaseTest {
         WebElement editBtn = getDriver().findElement(By.xpath("//a[text()='edit']"));
         wait.until(ExpectedConditions.elementToBeClickable(editBtn));
         editBtn.click();
-        Assert.assertTrue(getDriver().findElement(By.xpath("//button[@id='pa-entity-form-draft-btn']")).isDisplayed());
+        String newF1 = "40";
+        newData(getDriver(), newF1, "30");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//tr[@data-index='0']/td[2]")).getText(), newF1);
     }
 
 }
