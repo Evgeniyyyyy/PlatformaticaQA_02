094c3b6cb4666dd65bd4d5fbfc684c338075cd56
Andrey Teterin <robinhood.ex@gmail.com>
2021-05-27 16:28:24 -0400

    Refactoring

--- src/test/java/EntityArithmeticFunctionTest.java
+++ src/test/java/EntityArithmeticFunctionTest.java
@@ -9,38 +9,38 @@ import org.testng.annotations.Test;
 import utils.ProjectUtils;
 import utils.TestUtils;
 
-public class ArithmeticFunctionEditTest extends BaseTest {
+public class EntityArithmeticFunctionTest extends BaseTest {
 
-    public void newData (WebDriver driver, String x, String y) {
+    public void setNewData (WebDriver driver, String inputField1, String inputField2) {
         WebElement f1 = getDriver().findElement(By.xpath("//input[@data-field_name='f1']"));
         WebElement f2 = getDriver().findElement(By.xpath("//input[@data-field_name='f2']"));
         f1.clear();
-        f1.sendKeys(x);
+        f1.sendKeys(inputField1);
         f2.clear();
-        f2.sendKeys(y);
+        f2.sendKeys(inputField2);
+
         getDriver().findElement(By.xpath("//button[@id='pa-entity-form-save-btn']")).click();
     }
 
-
     public void createRecord(WebDriver driver) {
-        ProjectUtils.start(getDriver());
         WebElement arithmeticButton = getDriver().findElement(By.xpath("//p[text()=' Arithmetic Function ']/parent::a/parent::li"));
         TestUtils.scrollClick(getDriver(), arithmeticButton);
         getDriver().findElement(By.xpath("//i[text()='create_new_folder']")).click();
-        newData(getDriver(), "10", "20");
+        setNewData(getDriver(), "10", "20");
     }
 
     @Test
-    public void testCreateArithmeticFunctionRecord() {
+    public void testEditRecord() {
+        ProjectUtils.start(getDriver());
         WebDriverWait wait = new WebDriverWait(getDriver(), 10);
         createRecord(getDriver());
         getDriver().findElement(By.xpath("//div[@class='dropdown pull-left']")).click();
         WebElement editBtn = getDriver().findElement(By.xpath("//a[text()='edit']"));
         wait.until(ExpectedConditions.elementToBeClickable(editBtn));
         editBtn.click();
-        String newF1 = "40";
-        newData(getDriver(), newF1, "30");
-        Assert.assertEquals(getDriver().findElement(By.xpath("//tr[@data-index='0']/td[2]")).getText(), newF1);
-    }
+        String newField1 = "40";
+        setNewData(getDriver(), newField1, "30");
 
+        Assert.assertEquals(getDriver().findElement(By.xpath("//tr[@data-index='0']/td[2]")).getText(), newField1);
+    }
 }
