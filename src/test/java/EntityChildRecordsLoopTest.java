35fc608ec32689c2651424b7b845b94b43ca2c4b
Daria Altukhova <altukhova.daria@gmail.com>
2021-05-28 23:35:50 -0700

    View child Records Loop card test method

--- src/test/java/EntityChildRecordsLoopTest.java
+++ src/test/java/EntityChildRecordsLoopTest.java
@@ -1,7 +1,6 @@
 import base.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.*;
@@ -13,6 +12,8 @@ import static utils.TestUtils.scrollClick;
 
  public class EntityChildRecordsLoopTest extends BaseTest {
 
+     final static String URL = "https://ref2.eteam.work/";
+
      @Test
     public void testCreateChildRecordsLoopCard()  {
         final double startBalanceValue = 100;
@@ -20,7 +21,7 @@ import static utils.TestUtils.scrollClick;
         final double expectedEndBalance = startBalanceValue + cardAmountValue;
         final String cardItemValue = "book";
 
-        getDriver().get("https://ref2.eteam.work/");
+        getDriver().get(URL);
 
         ProjectUtils.login(getDriver());
 
@@ -58,5 +59,52 @@ import static utils.TestUtils.scrollClick;
         Assert.assertEquals(startBalanceAmount, startBalanceValue);
         Assert.assertEquals(endBalanceAmount, expectedEndBalance);
 
+    }
+
+    @Test
+    public void testViewChildRecordsLoopCard() {
+        final double startBalanceValue = 100;
+        final double cardAmountValue = 200;
+        final double expectedEndBalance = startBalanceValue + cardAmountValue;
+        final String cardItemValue = "book";
+
+        getDriver().get(URL);
+
+        ProjectUtils.login(getDriver());
+
+        WebElement childRecordsLoop = getDriver().findElement(By.xpath("//p[contains(text(),'Child records loop')]"));
+        scrollClick(getDriver(), childRecordsLoop);
+
+        List<WebElement> columnList = getDriver().findElements(By.xpath("//tbody/tr/td[@class='pa-list-table-th']"));
+        Assert.assertTrue(columnList.size() > 0);
+
+        int numberOfCards = columnList.size() / 2;
+
+        WebElement targetRowDiv = getDriver().findElement(By.xpath("//tbody/tr[" + numberOfCards + "]/td[4]/div[1]"));
+
+        WebElement lastCardDropdownMenu = targetRowDiv.findElement(By.xpath("button[1]"));
+        lastCardDropdownMenu.click();
+
+        WebElement viewEntity = targetRowDiv.findElement(By.xpath("ul/li/a[text() = 'view']"));
+        getWait().until(ExpectedConditions.visibilityOf(viewEntity));
+        viewEntity.click();
+
+        List<WebElement> balances = getDriver().findElements(By.className("pa-view-field"));
+        Assert.assertEquals(
+                balances.get(0).getText(),
+                String.format("%.2f", startBalanceValue) // "100.00"
+        );
+        Assert.assertEquals(
+                balances.get(1).getText(),
+                String.format("%.2f", expectedEndBalance) // "300.00"
+        );
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//tbody/tr[1]/td[2]")).getText().trim(),
+                String.format("%.2f", cardAmountValue) // "200.00"
+        );
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//tbody/tr[1]/td[3]")).getText().trim(),
+                cardItemValue // "book"
+        );
     }
 }
