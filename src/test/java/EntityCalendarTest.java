3fb055c9a391b3dd1132891cedd0b3e739a57ae1
Sergei Khatskevitch <65862926+skhat@users.noreply.github.com>
2021-05-29 22:16:11 -0700

    Refactoring EntityCalendarTest (#267)
    https://trello.com/c/TyZ2JSzp

--- src/test/java/EntityCalendarTest.java
+++ src/test/java/EntityCalendarTest.java
@@ -1,6 +1,5 @@
 import base.BaseTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
@@ -31,28 +30,36 @@ public class EntityCalendarTest extends BaseTest {
         TestUtils.scrollClick(getDriver(), findElement(By.id("pa-entity-form-save-btn")));
     }
 
-    private void enterToCalendarPageAndCreateRecord(String str, String text, String in, String decimal){
-        TestUtils.scrollClick(getDriver(), findElement(By.xpath("//p[contains (text(), 'Calendar')]")));
+    private void createCalendarRecord(List<String> list){
         findElement(By.xpath("//*[@class='card-icon']")).click();
 
-        findElement(By.id("string")).sendKeys(str);
-        findElement(By.id("text")).sendKeys(text);
+        findElement(By.id("string")).sendKeys(list.get(0));
+        findElement(By.id("text")).sendKeys(list.get(1));
         findElement(By.id("date")).click();
         findElement(By.id("datetime")).click();
-        findElement(By.id("int")).sendKeys(in);
-        findElement(By.id("decimal")).sendKeys(decimal);
+        findElement(By.id("int")).sendKeys(list.get(2));
+        findElement(By.id("decimal")).sendKeys(list.get(3));
 
         TestUtils.scrollClick(getDriver(), findElement(By.id("pa-entity-form-save-btn")));
     }
 
+    private void enterToCalendarPageAndCreateRecord(String str, String text, String in, String decimal){
+        enterToCalendarPage();
+        createCalendarRecord("StringExample", "TextExample", "12345", "0.1");
+    }
 
+    private void enterToCalendarPageAndCreateRecord(List<String> list){
+        enterToCalendarPage();
+        createCalendarRecord(list);
+    }
 
     @Test
     public void testDeletedRecordFromBin(){
+        final List<String> createRecordList = List.of("StringExampleCreateRecord", "TextExample", "1111", "0.20");
+
         ProjectUtils.start(getDriver());
 
-        enterToCalendarPage();
-        createCalendarRecord("StringExample", "TextExample", "12345", "0.1");
+        enterToCalendarPageAndCreateRecord(createRecordList);
 
         findElement(By.xpath("//*[contains(@href,'table&entity_id=32')]")).click();
         findElement(By.xpath("//*[contains(@class, 'btn btn-round')]")).click();
@@ -68,8 +75,10 @@ public class EntityCalendarTest extends BaseTest {
     @Ignore
     @Test
     public void testCalendarViewCreatedRecord() {
+        final List<String> createRecordList = List.of("StringExampleCreateRecord", "TextExample", "1111", "0.20");
+
         ProjectUtils.start(getDriver());
-        createCalendarRecord("StringExampleCreateRecord", "TextExample", "54321", "0.1");
+        enterToCalendarPageAndCreateRecord(createRecordList);
         TestUtils.scrollClick(getDriver(), findElement(By.xpath("//*[@class='fc-title']")));
         scroll(getDriver(), findElement(By.xpath("//*[@class='pa-view-field']")));
 
@@ -82,7 +91,7 @@ public class EntityCalendarTest extends BaseTest {
         
         ProjectUtils.start(getDriver());
 
-        enterToCalendarPageAndCreateRecord("StringExampleCreateRecord", "TextExample", "1111", "0.20");
+        enterToCalendarPageAndCreateRecord(createRecordList);
 
         findElement(By.xpath("//*[contains(@href,'table&entity_id=32')]")).click();
         
