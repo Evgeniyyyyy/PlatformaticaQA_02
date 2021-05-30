222692ec86e629a6c5db0ca4a853a4cd8b1fad27
Tatiana Kraskova <kraskova.qa@gmail.com>
2021-05-30 19:21:14 -0400

    1. private static final By 2. final String ENTERED_TEXT_FIELD 3. getDriver().findElement(recordedData).isDisplayed(); - delete it 4. Assert.assertTrue(getDriver().findElement(recordedData).isDisplayed()); - modify and get:         Assert.assertEquals(getDriver().findElement(recordedData).getText(), ENTERED_TEXT_FIELD);

--- src/test/java/EntityChevronSentRecordViaListTest.java
+++ src/test/java/EntityChevronSentRecordViaListTest.java
@@ -1,7 +1,6 @@
 import base.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,29 +9,27 @@ import static utils.TestUtils.scrollClick;
 
 public class EntityChevronSentRecordViaListTest extends BaseTest {
 
-    By createNewRecord = By.xpath("//i[contains(text(),'create_new_folder')]");
-    By textField = By.xpath("//textarea[@id='text']");
-    By saveButton = By.xpath("//button[@id='pa-entity-form-save-btn']");
-    By sentButton = By.xpath("//button[contains(text(), 'Sent')]");
-    By verifySentButton = By.xpath("//a[contains(text(),'Sent')]");
-    By recordedData = By.xpath("//a[contains(text(), 'firstExample')]");
-
+    private static final By createNewRecord = By.xpath("//i[contains(text(),'create_new_folder')]");
+    private static final By textField = By.xpath("//textarea[@id='text']");
+    private static final By saveButton = By.xpath("//button[@id='pa-entity-form-save-btn']");
+    private static final By sentButton = By.xpath("//button[contains(text(), 'Sent')]");
+    private static final By verifySentButton = By.xpath("//a[contains(text(),'Sent')]");
+    private static final By recordedData = By.xpath("//a[contains(text(), 'firstExample')]");
     private static final By CHEVRON_MENU = By.xpath("//p[contains(text(),'Chevron')]");
 
     @Test
     public void testCreateRecordVerifyThatExist() {
 
-        final String enteredTextField = "firstExample";
+        final String ENTERED_TEXT_FIELD = "firstExample";
 
         start(getDriver());
         scrollClick(getDriver(), getDriver().findElement(CHEVRON_MENU));
         getWait().until(ExpectedConditions.elementToBeClickable(getDriver().findElement(createNewRecord))).click();
-        getDriver().findElement(textField).sendKeys(enteredTextField);
+        getDriver().findElement(textField).sendKeys(ENTERED_TEXT_FIELD);
         getDriver().findElement(saveButton).click();
         getWait().until(ExpectedConditions.elementToBeClickable(getDriver().findElement(sentButton))).click();
         getWait().until(ExpectedConditions.elementToBeClickable(getDriver().findElement(verifySentButton))).click();
-        getDriver().findElement(recordedData).isDisplayed();
 
-        Assert.assertTrue(getDriver().findElement(recordedData).isDisplayed());
+        Assert.assertEquals(getDriver().findElement(recordedData).getText(), ENTERED_TEXT_FIELD);
     }
 }
