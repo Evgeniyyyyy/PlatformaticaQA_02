a5d701ef238b51601a3e767cab783d11d9138057
Andrey Teterin <robinhood.ex@gmail.com>
2021-06-01 10:07:02 -0400

    Entity > ArithmeticFunctionTest refactoting. Added fill_form checking. (#257)
    * Code refactoring. Added fill_form checking.
    
    * Edit fill_form checking.
    
    * Edit fill_form checking(second try).
    
    * Change waiter for viewer
    
    * Ignore EntityCalendarAddRecordTest

--- src/test/java/EntityArithmeticFunctionTest.java
+++ src/test/java/EntityArithmeticFunctionTest.java
@@ -1,9 +1,8 @@
 import base.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import utils.ProjectUtils;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,7 +12,12 @@ import java.util.List;
 
 public class EntityArithmeticFunctionTest extends BaseTest {
 
-    @Ignore
+    private static final By F1_FIELD = By.xpath("//input[@id='f1']");
+    private static final By F2_FIELD = By.xpath("//input[@id='f2']");
+    private static final By DIV_FIELD = By.xpath("//input[@id='div']");
+    private static final By CREATE_NEW = By.xpath("//i[text()='create_new_folder']");
+
+
     @Test
     public void testRecordSaveDraft(){
 
@@ -27,13 +31,12 @@ public class EntityArithmeticFunctionTest extends BaseTest {
         TestUtils.scrollClick(getDriver(),
                 findElement(By.xpath("//p[contains(text(),'Arithmetic Function')]")));
 
-        findElement(By.xpath("//i[contains(text(),'create_new_folder')]")).click();
+        findElement(CREATE_NEW).click();
 
-        findElement(By.xpath("//input[@id='f1']")).sendKeys(F1.toString());
-        findElement(By.xpath("//input[@id='f2']")).sendKeys(F2.toString());
+        findElement(F1_FIELD).sendKeys(F1.toString());
+        findElement(F2_FIELD).sendKeys(F2.toString());
 
-        getWait().until(ExpectedConditions.attributeToBe(
-                By.xpath("//input[@id='div']"), "value", DIV.toString()));
+        getWait().until(ExpectedConditions.attributeToBe(DIV_FIELD,"value", DIV.toString()));
 
         findElement(By.xpath("//button[@id='pa-entity-form-draft-btn']")).click();
 
@@ -47,29 +50,45 @@ public class EntityArithmeticFunctionTest extends BaseTest {
         }
     }
 
-    private void fillForm(String inputField1, String inputField2) {
-        WebElement f1 = findElement(By.xpath("//input[@data-field_name='f1']"));
-        WebElement f2 = findElement(By.xpath("//input[@data-field_name='f2']"));
-        f1.clear();
-        f1.sendKeys(inputField1);
-        f2.clear();
-        f2.sendKeys(inputField2);
+    private void sendKeysSlow(WebElement element, String text) {
+        if (!element.getAttribute("value").isEmpty()) {
+            element.clear();
+        }
+        String newValue = "";
 
-        Integer DIV = Integer.parseInt(inputField1) / Integer.parseInt(inputField2);
-        getWait().until(ExpectedConditions.attributeToBe(
-                By.xpath("//input[@id='div']"), "value", DIV.toString()));
+        for (char ch: text.toCharArray()) {
+            element.sendKeys(String.valueOf(ch));
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                }
 
+            newValue += ch;
+            getWait().until(ExpectedConditions.attributeToBe(element, "value", newValue));
+        }
+    }
+
+    private void fillForm(Integer inputField1, Integer inputField2) {
+        WebElement f1 = findElement(F1_FIELD);
+        WebElement f2 = findElement(F2_FIELD);
+        String expectedValueDIV = String.valueOf(inputField1/inputField2);
+
+        sendKeysSlow(f1, inputField1.toString());
+        sendKeysSlow(f2, inputField2.toString());
+
+        getWait().until(ExpectedConditions.attributeToBe(DIV_FIELD, "value", expectedValueDIV));
         findElement(By.xpath("//button[@id='pa-entity-form-save-btn']")).click();
     }
 
     private void createRecord() {
         TestUtils.scrollClick(getDriver(),
                 By.xpath("//p[text()=' Arithmetic Function ']/parent::a/parent::li"));
-        findElement(By.xpath("//i[text()='create_new_folder']")).click();
+        findElement(CREATE_NEW).click();
 
-        fillForm("20", "10");
+        fillForm(20, 10);
     }
-    @Ignore
+
     @Test
     public void testEditRecord() {
         final Integer F1 = 40;
@@ -81,8 +100,9 @@ public class EntityArithmeticFunctionTest extends BaseTest {
         createRecord();
 
         findElement(By.xpath("//div[@class='dropdown pull-left']")).click();
-        getWait().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='edit']"))).click();
-        fillForm(F1.toString(), F2.toString());
+        getWait().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='view']")));
+        findElement(By.xpath("//a[text()='edit']")).click();
+        fillForm(F1, F2);
 
         List<WebElement> columnList = findElements(By.xpath("//tbody/tr/td[@class = 'pa-list-table-th']"));
 
