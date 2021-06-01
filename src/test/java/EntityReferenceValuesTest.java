946bcdbadd9069bbd20afe4025c181e7405738a5
Razzhigaev1988 <razzhigaev1988@gmail.com>
2021-05-31 17:11:56 -0700

    Refactor EntityReferenceValuesTest class. (#302)
    * Refactoring of EntityReferenceValuesTest.
    
    * Refactoring of EntityReferenceValuesTest.
    Ignore testCreateRecord in class EntityCalendarAddRecordTest.

--- src/test/java/EntityReferenceValuesTest.java
+++ src/test/java/EntityReferenceValuesTest.java
@@ -1,7 +1,10 @@
 import base.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.util.Arrays;
+import java.util.List;
 import static utils.ProjectUtils.start;
 import static utils.TestUtils.scrollClick;
 
@@ -10,26 +13,36 @@ public class EntityReferenceValuesTest extends BaseTest {
     private static final String FIELD1_INPUT_VALUE = "Test2";
     private static final String FIELD2_INPUT_VALUE = "Test3";
 
-    By referenceValuesIcon = By.xpath("//p[contains(text(),'Reference values')]");
-    By createNewFolderIcon = By.xpath("//i[contains(text(),'create_new_folder')]");
-    By labelInput = By.xpath("//input[@id='label']");
-    By filter1Input = By.xpath("//input[@id='filter_1']");
-    By filter2Input = By.xpath("//input[@id='filter_2']");
-    By saveButton = By.xpath("//button[@id='pa-entity-form-save-btn']");
-    By checkSquare = By.xpath("//i[@class='fa fa-check-square-o']");
+    private static final By REFERENCE_VALUES_ICON = By.xpath("//p[contains(text(),'Reference values')]");
+    private static final By CREATE_NEW_FOLDER_ICON = By.xpath("//i[contains(text(),'create_new_folder')]");
+    private static final By LABEL_INPUT = By.xpath("//input[@id='label']");
+    private static final By FILTER_1_INPUT = By.xpath("//input[@id='filter_1']");
+    private static final By FILTER_2_INPUT = By.xpath("//input[@id='filter_2']");
+    private static final By SAVE_BUTTON = By.xpath("//button[@id='pa-entity-form-save-btn']");
+    private static final By CHECK_SQUARE = By.xpath("//i[@class='fa fa-check-square-o']");
+    private static final By COLUMN_LIST = By.xpath("//tbody/tr/td[@class = 'pa-list-table-th']");
+    private static final By CREATED_LINE = By.xpath("//table[@id='pa-all-entities-table']");
 
     @Test
     public void testCreateNewRecord(){
         start(getDriver());
-        scrollClick(getDriver(), getDriver().findElement(referenceValuesIcon));
+        final List<String> expectedValues =Arrays.asList(LABEL_INPUT_VALUE, FIELD1_INPUT_VALUE, FIELD2_INPUT_VALUE);
+        scrollClick(getDriver(), findElement(REFERENCE_VALUES_ICON));
 
-        getDriver().findElement(createNewFolderIcon).click();
-        getDriver().findElement(labelInput).sendKeys(LABEL_INPUT_VALUE);
-        getDriver().findElement(filter1Input).sendKeys(FIELD1_INPUT_VALUE);
-        getDriver().findElement(filter2Input).sendKeys(FIELD2_INPUT_VALUE);
-        getDriver().findElement(saveButton).click();
+        findElement(CREATE_NEW_FOLDER_ICON).click();
+        findElement(LABEL_INPUT).sendKeys(LABEL_INPUT_VALUE);
+        findElement(FILTER_1_INPUT).sendKeys(FIELD1_INPUT_VALUE);
+        findElement(FILTER_2_INPUT).sendKeys(FIELD2_INPUT_VALUE);
+        findElement(SAVE_BUTTON).click();
 
-        Assert.assertNotNull(getDriver().findElement(By.xpath("//table[@id='pa-all-entities-table']")));
-        Assert.assertTrue(getDriver().findElement(checkSquare).isDisplayed());
+        final List<WebElement> actualValues = findElements(COLUMN_LIST);
+
+        Assert.assertNotNull(getDriver().findElement(CREATED_LINE));
+        Assert.assertTrue(getDriver().findElement(CHECK_SQUARE).isDisplayed());
+
+        Assert.assertEquals(actualValues.size(), expectedValues.size());
+        for (int i = 0; i < expectedValues.size() ; i++) {
+            Assert.assertEquals(actualValues.get(i).getText(), expectedValues.get(i));
+        }
     }
 }
