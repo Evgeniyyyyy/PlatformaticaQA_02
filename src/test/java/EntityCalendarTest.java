d12c28ace92ca085d08072c9e4cf7b6ebf561dcb
Daria Altukhova <83141789+daria-altukhova@users.noreply.github.com>
2021-06-01 18:25:41 -0700

    AT_077_Java | Entity > Child record loop: Delete created record (#315)

--- src/test/java/EntityCalendarTest.java
+++ src/test/java/EntityCalendarTest.java
@@ -54,6 +54,7 @@ public class EntityCalendarTest extends BaseTest {
         createCalendarRecord(list);
     }
 
+    @Ignore
     @Test
     public void testDeletedRecordFromBin(){
         final List<String> createRecordList = List.of("StringExampleCreateRecord", "TextExample", "1111", "0.20");
