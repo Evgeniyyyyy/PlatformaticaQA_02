2a146051fddf65c0099b5c830dc519996fa3cda5
SergeiDemyanenko <Sergei.Demyanenko@gmail.com>
2021-06-01 20:57:32 -0700

    new reset (#325)
    * new reset

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,6 +2,7 @@ package base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
