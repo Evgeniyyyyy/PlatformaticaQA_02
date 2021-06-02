ca920f1776dbb608b6d869037e26e67ea32e51f8
ArinaJur <66705803+ArinaJur@users.noreply.github.com>
2021-06-02 18:56:23 -0400

    testEditRecord() added. Static final locators deleted. Custom waiters were added to methods (#340)
    Co-authored-by: AniriRuj <Rjntyjr12#>

--- src/test/java/EntityEventsChain1Test.java
+++ src/test/java/EntityEventsChain1Test.java
@@ -4,43 +4,50 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.TestUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 import static utils.ProjectUtils.start;
+import static utils.TestUtils.movingIsFinished;
 import static utils.TestUtils.scrollClick;
 
 public class EntityEventsChain1Test extends BaseTest {
 
-    private static final By EVENTS_CHAIN1_MENU = By.xpath("//p[contains(.,'Events Chain 1')]");
-    private static final By CREATE_NEW_FOLDER_BUTTON = By.xpath("//i[text() = 'create_new_folder']");
-    private static final By F1_FIELD = By.id("f1");
-    private static final By F10_FIELD = By.id("f10");
-    private static final By SAVE_BUTTON = By.id("pa-entity-form-save-btn");
-    private static final By CELLS = By.xpath("//table[@id = 'pa-all-entities-table']/tbody/tr/td/a");
-    private static final By ICON = By.xpath("//tbody/tr/td[1]/i");
-
     private void clickEventsChain1Menu(){
-        scrollClick(getDriver(), getDriver().findElement(EVENTS_CHAIN1_MENU));
-        getWait().until(ExpectedConditions.elementToBeClickable(getDriver().findElement(CREATE_NEW_FOLDER_BUTTON)));
+        scrollClick(getDriver(), getDriver().findElement(By.xpath("//p[contains(.,'Events Chain 1')]")));
+        getWait().until(ExpectedConditions.elementToBeClickable(
+                getDriver().findElement(By.xpath("//i[text() = 'create_new_folder']"))));
     }
     private void clickCreateNewFolderButton() {
-        getDriver().findElement(CREATE_NEW_FOLDER_BUTTON).click();
+        getDriver().findElement(By.xpath("//i[text() = 'create_new_folder']")).click();
+        getWait().until(ExpectedConditions.presenceOfElementLocated(By.id("f1")));
     }
 
     private void inputF1Value(String f1Value){
-        getDriver().findElement(F1_FIELD).sendKeys(f1Value);
-        getWait().until(ExpectedConditions.attributeToBeNotEmpty(getDriver().findElement(F10_FIELD), "value"));
+        getDriver().findElement(By.id("f1")).sendKeys(f1Value);
+        getWait().until(ExpectedConditions.attributeToBeNotEmpty(getDriver().findElement(By.id("f10")), "value"));
     }
 
     private void clickSaveButton(){
-        getDriver().findElement(SAVE_BUTTON).click();
+        getDriver().findElement(By.id("pa-entity-form-save-btn")).click();
+        getWait().until(ExpectedConditions.elementToBeClickable(
+                getDriver().findElement(By.xpath("//i[text() = 'create_new_folder']"))));
+    }
+
+    private void createNewRecord() {
+        getDriver().findElement(By.xpath("//i[contains(text(), 'create_new_folder')]")).click();
+        getDriver().findElement(By.id("f1")).sendKeys("5");
+        getWait().until(ExpectedConditions.attributeToBeNotEmpty(getDriver().findElement(By.id("f10")), "value"));
+
+        getDriver().findElement(By.id("pa-entity-form-save-btn")).click();
     }
 
     private List<WebElement> getCells(){
-        return getDriver().findElements(CELLS);
+        return getDriver().findElements(By.xpath("//table[@id = 'pa-all-entities-table']/tbody/tr/td/a"));
     }
 
     private List<String> getRowValues() {
@@ -54,7 +61,7 @@ public class EntityEventsChain1Test extends BaseTest {
     }
 
     private String getAttributeClass() {
-        return getDriver().findElement(ICON).getAttribute("class");
+        return getDriver().findElement(By.xpath("//tbody/tr/td[1]/i")).getAttribute("class");
     }
 
     @Test
@@ -70,6 +77,48 @@ public class EntityEventsChain1Test extends BaseTest {
 
         Assert.assertEquals(getCells().size(), expectedValues.size());
         Assert.assertEquals(getRowValues(), expectedValues);
+        Assert.assertEquals(getAttributeClass(), "fa fa-check-square-o");
+    }
+
+    @Test
+    public void testEditRecord() {
+        List<String> expectedValues = List.of("2", "4", "8", "16", "32", "64", "128", "256", "512", "1024");
+
+        start(getDriver());
+
+        TestUtils.scrollClick(getDriver(), By.xpath("//p[contains(text(), 'Events Chain 1')]"));
+
+        createNewRecord();
+
+        String text = getDriver().findElement(By.xpath("//span[@class = 'pagination-info']")).getText();
+
+        Assert.assertEquals(text, "Showing 1 to 1 of 1 rows");
+
+        List<WebElement> cells = getDriver().findElements(By.xpath("//td[@class = 'pa-list-table-th']"));
+        List<String> oldValues = new ArrayList<>();
+        for(WebElement cell: cells){
+            oldValues.add(cell.getText());
+        }
+
+        getDriver().findElement(By.xpath("//i[contains(., 'menu')]")).click();
+
+        getWait().until(movingIsFinished(By.xpath("//a[contains(., 'edit')]"))).click();
+
+        getDriver().findElement(By.id("f10")).clear();
+        getDriver().findElement(By.id("f1")).clear();
+        getDriver().findElement(By.id("f1")).sendKeys("2");
+        getWait().until(ExpectedConditions.attributeToBeNotEmpty(getDriver().findElement(By.id("f10")), "value"));
+
+        getDriver().findElement(By.id("pa-entity-form-save-btn")).click();
+
+        List<WebElement> cells1 = getDriver().findElements(By.xpath("//td[@class = 'pa-list-table-th']"));
+        List<String> actualValues = new ArrayList<>();
+        for(WebElement cell: cells1){
+            actualValues.add(cell.getText());
+        }
+
+        Assert.assertEquals(actualValues, expectedValues);
+        Assert.assertNotEquals(actualValues, oldValues);
         Assert.assertEquals(getAttributeClass(), "fa fa-check-square-o");
     }
 }
