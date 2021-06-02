67382969c70936234c29461dbfc0cd283146d2fd
Yerlan Nurgaliyev <75294179+NYerlan@users.noreply.github.com>
2021-06-01 18:50:03 -0700

    Refactoring - TC_004.02_Java | Entity > Board: Edit record (#314)

--- src/test/java/EntityBoardEditRecordTest.java
+++ src/test/java/EntityBoardEditRecordTest.java
@@ -4,10 +4,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import utils.ProjectUtils;
 import utils.TestUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static utils.TestUtils.scrollClick;
 
 public class EntityBoardEditRecordTest extends BaseTest {
@@ -17,20 +21,39 @@ public class EntityBoardEditRecordTest extends BaseTest {
 
     private static final By ENTITY_BOARD_MENU = By.xpath("//div[@id='menu-list-parent']//li[10]/a/i");
     private static final By CREATE_NEW_RECORD = By.xpath("//i[contains (text(), 'create_new_folder')]");
-    private static final By TEXT_INPUT_AREA = By.xpath("//p//span/textarea");
+    private static final By TEXT_INPUT_AREA = By.xpath("//textarea[@id='text']");
     private static final By INT_INPUT_AREA = By.xpath("//span/input[@id='int']");
     private static final By SAVE_BUTTON = By.xpath("//button[@id='pa-entity-form-save-btn']");
     private static final By LIST_BUTTON = By.xpath("//li//a[@class='nav-link ']/i[@class='material-icons']");
     private static final By ACTIONS_BUTTON = By.xpath("//tbody/tr//td//button[@type='button']");
     private static final By EDIT_BUTTON = By.xpath("//tbody/tr//td//li[2]/a");
+    private static final By CELLS = By.xpath("//tbody//td[@class='pa-list-table-th']");
+    private static final By ICON = By.xpath("//tbody/tr/td[1]/i");
+
+    private List<WebElement> getCells() {
+        return getDriver().findElements(CELLS);
+    }
+
+    private List<String> getRowValues() {
+        List<String> actualValues = new ArrayList<>();
+
+        for(WebElement cell : getCells()) {
+            actualValues.add(cell.getText());
+        }
+
+        return actualValues;
+    }
+
+    private String getAttributeClass() {
+        return getDriver().findElement(ICON).getAttribute("class");
+    }
 
     private void createRecord() {
-        WebDriverWait wait = new WebDriverWait(getDriver(), 10);
 
         ProjectUtils.start(getDriver());
 
         TestUtils.scrollClick(getDriver(), getDriver().findElement(ENTITY_BOARD_MENU));
-        wait.until(ExpectedConditions.elementToBeClickable(getDriver()
+        getWait().until(ExpectedConditions.elementToBeClickable(getDriver()
                 .findElement(CREATE_NEW_RECORD)))
                 .click();
         getDriver().findElement(TEXT_INPUT_AREA).sendKeys("Some text here...");
@@ -39,21 +62,21 @@ public class EntityBoardEditRecordTest extends BaseTest {
         TestUtils.scrollClick(getDriver(),findSaveButton);
     }
 
-        @Ignore
         @Test
-        public void testEditCreatedRecord() {
-            final String TEXT = "any text";
+        public void testEditRecord() {
+
+            final List<String> expectedValues = Arrays.asList(
+                    "Pending", NEW_TEXT, NEW_INT, "0.00", "", "", "", "apptester1@tester.test");
 
             createRecord();
-
-            WebDriverWait wait = new WebDriverWait(getDriver(), 10);
 
             scrollClick(getDriver(), getDriver().findElement(ENTITY_BOARD_MENU));
             getDriver().findElement(LIST_BUTTON).click();
             getDriver().findElement(ACTIONS_BUTTON).click();
 
-            wait.until(ExpectedConditions.elementToBeClickable(EDIT_BUTTON));
+            getWait().until(ExpectedConditions.elementToBeClickable(EDIT_BUTTON));
             getDriver().findElement(EDIT_BUTTON).click();
+            getWait().until(ExpectedConditions.elementToBeClickable(TEXT_INPUT_AREA));
             WebElement textField = getDriver().findElement(TEXT_INPUT_AREA);
             textField.clear();
             textField.sendKeys(NEW_TEXT);
@@ -64,9 +87,8 @@ public class EntityBoardEditRecordTest extends BaseTest {
             WebElement SaveButton = getDriver().findElement(SAVE_BUTTON);
             TestUtils.scrollClick(getDriver(),SaveButton);
 
-            String resultText = getDriver().findElement(By.xpath("//tbody//td[3]/a")).getText();
-            Assert.assertEquals(resultText, NEW_TEXT);
-            String resultInt = getDriver().findElement(By.xpath("//tbody//td[4]/a")).getText();
-            Assert.assertEquals(resultInt, NEW_INT);
+            Assert.assertEquals(getAttributeClass(), "fa fa-check-square-o");
+            Assert.assertEquals(getCells().size(), expectedValues.size());
+            Assert.assertEquals(getRowValues(), expectedValues);
         }
 }
