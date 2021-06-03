e4082df47277459c328c945014121121eac5c7c2
Mikhail Mir <microf2000@rambler.ru>
2021-06-03 07:59:58 -0700

    refactoring EntityArithmeticInlineTest (#347)

--- src/test/java/EntityArithmeticInlineTest.java
+++ src/test/java/EntityArithmeticInlineTest.java
@@ -1,12 +1,10 @@
 import base.BaseTest;
-import constants.EntityAssignConstants;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
-import utils.ProjectUtils;
+import static utils.ProjectUtils.*;
 import utils.TestUtils;
 import constants.EntityArithmeticInlineConstants;
 
@@ -21,40 +19,40 @@ public class EntityArithmeticInlineTest extends BaseTest {
                 By.xpath("//input[@id='div']"),
                 "value",
                 EntityArithmeticInlineConstants.DIV.toString()));
-        findElement(EntityArithmeticInlineConstants.ARITHMETIC_BUTTON_SAVE).click();
+        clickSave(getDriver());
     }
 
     private void createRecord() {
-        findElement(EntityArithmeticInlineConstants.ARITHMETIC_ADD_CARD).click();
+        findElement(EntityArithmeticInlineConstants.ADD_CARD).click();
         fillForm(EntityArithmeticInlineConstants.F1.toString(), EntityArithmeticInlineConstants.F2.toString());
     }
 
     private void viewAction() {
-        WebElement button_action = findElement(EntityArithmeticInlineConstants.ARITHMETIC_ACTION_BUTTON);
+        WebElement button_action = findElement(EntityArithmeticInlineConstants.ACTION_BUTTON);
         button_action.click();
-        WebElement view_action = findElement(EntityArithmeticInlineConstants.ARITHMETIC_ACTION_VIEW);
+        WebElement view_action = findElement(EntityArithmeticInlineConstants.ACTION_VIEW);
         view_action.click();
     }
 
     private void viewActionClose() {
-        WebElement button_close_window = findElement(EntityArithmeticInlineConstants.ARITHMETIC_VIEW_WINDOW_CLOSE);
+        WebElement button_close_window = findElement(EntityArithmeticInlineConstants.VIEW_WINDOW_CLOSE);
         button_close_window.click();
     }
 
-    @Ignore
     @Test
     public void testViewRecord() {
 
-        ProjectUtils.start(getDriver());
+        start(getDriver());
 
         TestUtils.scrollClick(getDriver(),
-                findElement(EntityArithmeticInlineConstants.LINK_ARITHMETIC_ENTITY));
+                findElement(EntityArithmeticInlineConstants.LINK_ENTITY));
 
-        createRecord();
+        clickCreateRecord(getDriver());
+        fillForm(EntityArithmeticInlineConstants.F1.toString(), EntityArithmeticInlineConstants.F2.toString());
         viewAction();
 
-        getWait().until(ExpectedConditions.presenceOfElementLocated(EntityArithmeticInlineConstants.ARITHMETIC_ACTION_VIEW_TITLE));
-        List<WebElement> actualList = findElements(EntityArithmeticInlineConstants.ARITHMETIC_RESULT_LIST);
+        getWait().until(ExpectedConditions.presenceOfElementLocated(EntityArithmeticInlineConstants.ACTION_VIEW_TITLE));
+        List<WebElement> actualList = findElements(EntityArithmeticInlineConstants.RESULT_LIST);
         List<Integer> expectedList = EntityArithmeticInlineConstants.expectedList;
 
         Assert.assertEquals(actualList.size(), expectedList.size());
