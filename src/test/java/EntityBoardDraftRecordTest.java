2ebe9da4ec065fcc77de9f87eb37093293abba14
Daria-Razzhigaeva <82791007+Daria-Razzhigaeva@users.noreply.github.com>
2021-06-04 16:39:00 -0700

    IGNORED: testEditRecord(EntityBoardEditRecordTest), testViewDraftRecord(EntityBoardViewDraftRecordTest), testDeleteRecordFromRecycleBin(EntityBoardDraftRecordTest) (#376)
    * Added dependency on testViewRecord from testCreateRecord
    
    * Added dependency on testViewRecord from testCreateRecord
    
    * Added dependency on testViewRecord from testCreateRecord
    
    * added new Assert
    
    * Added ignors for
    testEditRecord(EntityBoardEditRecordTest),
    testViewDraftRecord(EntityBoardViewDraftRecordTest)
    
    * Delete EntityGanttTest.java
    
    * Added an ignor for
    testDeleteRecordFromRecycleBin(EntityBoardDraftRecordTest)
    
    * Revert "Delete EntityGanttTest.java"
    
    This reverts commit 47e8cf6b
    
    * Delete EntityGanttTest.java
    
    Co-authored-by: Oleksandr Burdeinyi <62510739+Burdishka@users.noreply.github.com>

--- src/test/java/EntityBoardDraftRecordTest.java
+++ src/test/java/EntityBoardDraftRecordTest.java
@@ -102,6 +102,7 @@ public class EntityBoardDraftRecordTest extends BaseTest {
         }
     }
 
+    @Ignore
     @Test
     public void testDeleteRecordFromRecycleBin() {
 
--- src/test/java/EntityBoardViewDraftRecordTest.java
+++ src/test/java/EntityBoardViewDraftRecordTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import utils.ProjectUtils;
 import utils.TestUtils;
@@ -59,7 +60,7 @@ public class EntityBoardViewDraftRecordTest extends BaseTest {
         return list;
     }
 
-
+    @Ignore
     @Test
     public void testViewDraftRecord() {
         final List<String> expectedValues = Arrays.asList(
