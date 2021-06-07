022dad195d2b80d51c7eb4a8d4fe707bf0211144
ValeriiSarukhanov <82187165+ValeriiSarukhanov@users.noreply.github.com>
2021-06-06 21:12:15 -0400

    AT_089_Java | Entity > Parent: Reorder records (#406)
    * Reorder record

--- src/test/java/EntityParentSTest.java
+++ src/test/java/EntityParentSTest.java
@@ -1,6 +1,8 @@
 import base.DriverPerClassBaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -25,7 +27,7 @@ public class EntityParentSTest extends DriverPerClassBaseTest {
         ProjectUtils.clickSave(getDriver());
     }
 
-    private void createNewRecord() {
+    private void editRecord() {
 
         getDriver().findElement(By.id("string")).clear();
         getDriver().findElement(By.id("string")).sendKeys("Hello for everyone");
@@ -54,10 +56,10 @@ public class EntityParentSTest extends DriverPerClassBaseTest {
                 "//a[@href='index.php?action=action_list&list_type=table&entity_id=57']")).click();
     }
 
-    private final List<String> expect = List.of(
+    private final List<String> world = List.of(
             "Hello world", "Be healthy", "123", "456.98", "", "");
 
-    private final List<String> expect1 = List.of(
+    private final List<String> everyone = List.of(
             "Hello for everyone", "Peace to all", "345", "345.67", "", "");
 
     @Test
@@ -74,7 +76,7 @@ public class EntityParentSTest extends DriverPerClassBaseTest {
 
         Assert.assertEquals(icon.getAttribute("class"), "fa fa-check-square-o");
         for (int i = 0; i < result.size(); i++) {
-            Assert.assertEquals(result.get(i).getText(), expect.get(i));
+            Assert.assertEquals(result.get(i).getText(), world.get(i));
         }
     }
 
@@ -89,7 +91,7 @@ public class EntityParentSTest extends DriverPerClassBaseTest {
         List<WebElement> row = findElements(By.xpath("//span[@class='pa-view-field']"));
         Assert.assertEquals(row.size(), 6);
         for (int i = 0; i < row.size(); i++) {
-            Assert.assertEquals(row.get(i).getText(), expect.get(i));
+            Assert.assertEquals(row.get(i).getText(), world.get(i));
         }
         getDriver().findElement(By.xpath("//i[text()='clear']")).click();
     }
@@ -103,11 +105,11 @@ public class EntityParentSTest extends DriverPerClassBaseTest {
         findElement(By.xpath("//button/i[@class='material-icons']")).click();
         TestUtils.jsClick(getDriver(), getDriver().findElement(By.xpath("//a[text()='edit']")));
 
-        createNewRecord();
+        editRecord();
 
         List<WebElement> result = getDriver().findElements(By.xpath("//tbody/tr/td/a"));
         for (int i = 0; i < result.size(); i++) {
-            Assert.assertEquals(result.get(i).getText(), expect1.get(i));
+            Assert.assertEquals(result.get(i).getText(), everyone.get(i));
         }
 
         WebElement newRecord = findElement(By.tagName("tbody"));
@@ -125,7 +127,7 @@ public class EntityParentSTest extends DriverPerClassBaseTest {
         WebElement record1 = getDriver().findElement(By.xpath("//tr[@data-index='1']"));
         record1.getText();
 
-        List <WebElement> fields = findElements(By.xpath("//tbody/tr"));
+        List<WebElement> fields = findElements(By.xpath("//tbody/tr"));
         Assert.assertEquals(fields.size(), 2);
         Assert.assertNotEquals(record, record1);
 
@@ -135,7 +137,7 @@ public class EntityParentSTest extends DriverPerClassBaseTest {
 
         List<WebElement> result = getDriver().findElements(By.xpath("//tbody/tr/td/a"));
         for (int i = 0; i < result.size(); i++) {
-            Assert.assertEquals(result.get(i).getText(), expect.get(i));
+            Assert.assertEquals(result.get(i).getText(), world.get(i));
         }
 
         findElement(By.xpath("//input[@type='text']")).clear();
@@ -148,7 +150,40 @@ public class EntityParentSTest extends DriverPerClassBaseTest {
 
         List<WebElement> result1 = getDriver().findElements(By.xpath("//tbody/tr/td/a"));
         for (int i = 0; i < result1.size(); i++) {
-            Assert.assertEquals(result1.get(i).getText(), expect1.get(i));
+            Assert.assertEquals(result1.get(i).getText(), everyone.get(i));
         }
+    }
+
+    @Test(dependsOnMethods = "testSearchRecord")
+    public void testReorderRecord() {
+
+
+        findElement(By.xpath("//input[@type='text']")).clear();
+        findElement(By.xpath("//i[text()='format_line_spacing']")).click();
+
+        List<WebElement> record = getDriver().findElements(By.xpath("//tbody/tr/td/a"));
+        Assert.assertEquals(record.get(0).getText(), everyone.get(0));
+
+        Actions actions = new Actions(getDriver());
+        WebElement row = findElement(By.xpath("//tbody/tr"));
+        actions.moveToElement(row).clickAndHold(row).dragAndDropBy(row, 0, 20);
+        Action swapRow = actions.build();
+        swapRow.perform();
+
+        List<WebElement> record1 = getDriver().findElements(By.xpath("//tbody/tr/td/a"));
+        Assert.assertEquals(record1.get(0).getText(), world.get(0));
+
+        getDriver().findElement(By.xpath("//i[@class='fa fa-toggle-off']")).click();
+
+        List<WebElement> viewToggle = getDriver().findElements(By.xpath("//div/span/a"));
+        Assert.assertEquals(viewToggle.get(0).getText(), world.get(0));
+
+        WebElement card = getDriver().findElement(By.id("customId_0"));
+        actions.moveToElement(card).clickAndHold(card).dragAndDropBy(card, 0, 100);
+        Action swapCard = actions.build();
+        swapCard.perform();
+
+        List<WebElement> viewCard = getDriver().findElements(By.xpath("//div/span/a"));
+        Assert.assertEquals(viewCard.get(0).getText(), everyone.get(0));
     }
 }
