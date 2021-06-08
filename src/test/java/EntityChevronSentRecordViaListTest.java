c52736d870b4265aede300383b84c82e49bbfe95
Tatiana Kraskova <kraskova.qa@gmail.com>
2021-06-08 00:34:04 -0400

    add more methods and refactoring

--- src/test/java/EntityChevronSentRecordViaListTest.java
+++ src/test/java/EntityChevronSentRecordViaListTest.java
@@ -6,40 +6,74 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.ProjectUtils;
 import utils.TestUtils;
+
+import java.util.ArrayList;
 import java.util.List;
+
 import static utils.ProjectUtils.clickSave;
 
 public class EntityChevronSentRecordViaListTest extends BaseTest {
 
-    final String enteredData = "firstExample";
+    private static final String ENTERED_DATA = "firstExample";
 
     private void createNewRecord() {
-        ProjectUtils.start(getDriver());
-        TestUtils.scrollClick(getDriver(), By.xpath("//p[contains(text(),'Chevron')]"));
         getWait().until(ExpectedConditions.elementToBeClickable(getDriver()
                 .findElement(By.xpath("//i[contains(text(),'create_new_folder')]"))))
                 .click();
-        findElement(By.id("text")).sendKeys(enteredData);
+        findElement(By.id("text")).sendKeys(ENTERED_DATA);
+        clickSave(getDriver());
     }
 
-    public void sentCreatedRecord() {
+    private void sentCreatedRecord() {
         findElement(By.xpath("//button[contains(text(),'Sent')]")).click();
     }
 
-    private void verifyThatCreatedRecordIsInSentFolder() {
-        findElement(By.xpath("//a[contains(text(),'Sent')]")).click();
+    private void clickSentMenu() {
+        findElement(By.xpath("//a[contains(.,'Sent')]")).click();
+    }
+
+    private List<WebElement> getCellsValues(){
+        return findElements(By.xpath("//tbody/tr/td[@class = 'pa-list-table-th']/a"));
+    }
+
+    private List<String> getValues() {
+        List<String> values = new ArrayList<>();
+        for (int i = 1; i < getCellsValues().size(); i++) {
+            values.add(getCellsValues().get(i).getText());
+        }
+        return values;
+    }
+
+    private List<WebElement> getCells(){
+        return findElements(By.xpath("//tbody/tr/td[@class = 'pa-list-table-th']"));
+    }
+
+    private List<String> getLastTwoCellsValues() {
+        List<String> values = new ArrayList<>();
+        for (int i = getCells().size() - 2; i < getCells().size(); i++) {
+            values.add(getCells().get(i).getText());
+        }
+        return  values;
     }
 
     @Test
     public void testSentRecordViaList() {
+        ProjectUtils.start(getDriver());
+        TestUtils.scrollClick(getDriver(), By.xpath("//p[contains(text(),'Chevron')]"));
+
         createNewRecord();
-        clickSave(getDriver());
-        final List<Object> expectedRecordsRow = List.of(enteredData);
-        final List<WebElement> actualList = findElements(By.xpath("//tbody/tr[1]"));
-        Assert.assertEquals(actualList.size(), expectedRecordsRow.size());
+
+        List<String> oldValues = getValues();
+        oldValues.addAll(getLastTwoCellsValues());
+
         sentCreatedRecord();
-        verifyThatCreatedRecordIsInSentFolder();
-        final List<Object> expectedRecordsRowAfterSent = List.of(enteredData);
-        Assert.assertEquals(actualList.size(), expectedRecordsRowAfterSent.size());
+        clickSentMenu();
+
+        Assert.assertEquals(getCellsValues().get(0).getText(), "Sent");
+
+        List<String> newValues = getValues();
+        newValues.addAll(getLastTwoCellsValues());
+
+        Assert.assertEquals(newValues, oldValues);
     }
 }
