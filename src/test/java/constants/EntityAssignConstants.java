26b87283817f27ef6d635cc91aec46d46ecf71aa
Mikhail Mir <microf2000@rambler.ru>
2021-06-11 19:08:25 -0700

    Refactoring Entity Assign (#462)
    * refactoring EntityAssignTest

--- src/test/java/constants/EntityAssignConstants.java
+++ src/test/java/constants/EntityAssignConstants.java
@@ -4,19 +4,18 @@ import org.openqa.selenium.By;
 
 public class EntityAssignConstants {
 
-    public static final By LINK_ASSIGN_ENTITY = By.xpath("//a[@href=\"index.php?action=action_list&entity_id=37&mod=2\"]");
-    public static final By LINK_ASSIGN_ICON = By.xpath("//a[@href=\"index.php?action=action_list&entity_id=37&mod=2\"]/i");
-    public static final String URL = "https://ref2.eteam.work/index.php?action=login";
-    public static final String URL_ASSIGN = "https://ref2.eteam.work/index.php?action=action_list&entity_id=37&mod=2";
+    public static final By LINK_ASSIGN_ENTITY = By.xpath("//p[text()=' Assign ']");
 
-    public static final By ASSIGN_ADD_CARD = By.xpath("//div[@class=\"card-icon\"]");
     public static final By ASSIGN_STRING_FIELD = By.id("string");
     public static final By ASSIGN_TEXT_FIELD = By.id("text");
     public static final By ASSIGN_DATE_FIELD = By.id("date");
     public static final By ASSIGN_INT_FIELD = By.id("int");
     public static final By ASSIGN_DECIMAL_FIELD = By.id("decimal");
+    public static final By ASSIGN_DATETIME_FIELD = By.id("datetime");
     public static final By ASSIGN_BUTTON_SAVE = By.id("pa-entity-form-save-btn");
     public static final By ASSIGN_BUTTON_SAVE_DRAFT = By.id("pa-entity-form-draft-btn");
+
+    public static final By ASSIGN_GET_CONTANER = By.className("card-body");
 
     public static final By ASSIGN_TOGGLE_LIST_ACTION = By.xpath("//a[@href=\"index.php?action=action_list&list_type=table&entity_id=37\"]");
     public static final By ASSIGN_TOGGLE_ORDER_ACTION = By.xpath("//a[@href=\"index.php?action=action_list&list_type=table&entity_id=37&draggable=1\"]");
