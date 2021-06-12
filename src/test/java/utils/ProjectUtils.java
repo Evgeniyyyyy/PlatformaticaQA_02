d2601f9f3a04e3d2d709d9a38f04bb76a08715c4
ValeriiSarukhanov <82187165+ValeriiSarukhanov@users.noreply.github.com>
2021-06-11 23:42:18 -0400

    RF_Java | ProjectUtils > add methods (#465)
    * Refactor Add methods

--- src/test/java/utils/ProjectUtils.java
+++ src/test/java/utils/ProjectUtils.java
@@ -3,6 +3,7 @@ package utils;
 import base.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -12,7 +13,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 import java.util.Properties;
+
+import static utils.TestUtils.jsClick;
 
 public class ProjectUtils {
 
@@ -63,5 +67,28 @@ public class ProjectUtils {
         TestUtils.jsClick(driver,
                 wait.until(ExpectedConditions.elementToBeClickable(
                         By.xpath("//a[contains(text(),'!!! Reset all for my user !!!')]"))));
+    }
+
+    public static List<WebElement> ACTUAL_RESULT(WebDriver driver) {
+        return List.of(driver.findElement(By.xpath("//tbody/tr/td/a")));
+    }
+
+    public static void getEntity(WebDriver driver, String name) {
+        jsClick(driver, driver.findElement(By.xpath("//p[text()= ' " + name + " ']")));
+    }
+
+    public static void clickActionsView(WebDriver driver) {
+        driver.findElement(By.xpath("//button/i[@class='material-icons']")).click();
+        TestUtils.jsClick(driver, driver.findElement(By.xpath("//a[text()='view']")));
+    }
+
+    public static void clickActionsEdit(WebDriver driver) {
+        driver.findElement(By.xpath("//button/i[@class='material-icons']")).click();
+        TestUtils.jsClick(driver, driver.findElement(By.xpath("//a[text()='edit']")));
+    }
+
+    public static void clickActionsDelete(WebDriver driver) {
+        driver.findElement(By.xpath("//button/i[@class='material-icons']")).click();
+        TestUtils.jsClick(driver, driver.findElement(By.xpath("//a[text()='delete']")));
     }
 }
