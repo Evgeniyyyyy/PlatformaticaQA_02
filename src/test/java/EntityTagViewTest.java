9a0034ba586d20f4cd4b82c3dd86ddf555546f14
Vadym Koroliuk <30486462+Wis13@users.noreply.github.com>
2021-06-14 18:10:52 -0700

    Vk entity tag view (#498)
     Refactoring Test EntityTagView

--- src/test/java/EntityTagViewTest.java
+++ src/test/java/EntityTagViewTest.java
@@ -6,67 +6,52 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.ProjectUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static utils.ProjectUtils.*;
 import static utils.TestUtils.jsClick;
 import static utils.TestUtils.scrollClick;
 
 
 public class EntityTagViewTest extends BaseTest {
 
-    public boolean isElementPresent(By element) {
-        try {
-            getDriver().findElement(element);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
+    private static final String STRING = "String";
+    private static final String TEXT = "Text";
+    private static final String INT = "23";
+    private static final String DECIMAL = "23.23";
+    private static final List<String> EXPECTED_RESULT = List.of(STRING, TEXT, INT, DECIMAL, "", "");
+    private static final By fieldDate = By.xpath("//input[@id='date']");
+    private static final By fieldDateTime = By.xpath("//input[@id='datetime']");
+
+    private void fillField(By field){
+        getDriver().findElement(field).click();
+        getDriver().findElement(field).clear();
+        getDriver().findElement(field).sendKeys("");
     }
 
-    public void createNewEntity(){
-        ProjectUtils.start(getDriver());
-        WebElement tag = getDriver().findElement(By.xpath("//p[contains(.,'Tag')]"));
-        scrollClick(getDriver(), tag);
-        getDriver().findElement(By.xpath("//i[contains(.,'create_new_folder')]")).click();
-        getDriver().findElement(By.id("string")).sendKeys("String");
-        getDriver().findElement(By.id("text")).sendKeys("Text");
-        getDriver().findElement(By.id("int")).sendKeys("23");
-        getDriver().findElement(By.id("decimal")).sendKeys("23.23");
-        getDriver().findElement(By.id("date")).click();
-        jsClick(getDriver(), getDriver().findElement(By.cssSelector(".today")));
-        getDriver().findElement(By.id("datetime")).click();
-        jsClick(getDriver(), getDriver().findElement(By.cssSelector(".today")));
-        getDriver().findElement(By.id("pa-entity-form-save-btn")).click();
+
+    private void createNewEntity(){
+        getEntity(getDriver(), "Tag");
+        clickCreateRecord(getDriver());
+        getDriver().findElement(By.id("string")).sendKeys(STRING);
+        getDriver().findElement(By.id("text")).sendKeys(TEXT);
+        getDriver().findElement(By.id("int")).sendKeys(INT);
+        getDriver().findElement(By.id("decimal")).sendKeys(DECIMAL);
+        fillField(fieldDate);
+        fillField(fieldDateTime);
+        clickSave(getDriver());
         getDriver().findElement(By.xpath("//p[contains(.,'Home')]")).click();
     }
 
     @Test
     public void testViewRecord(){
-        By MENU_ENTITY_TAG = By.xpath("//p[contains(.,'Tag')]");
-        By BUTTON_ACTION = By.cssSelector(".btn-round");
-        By MENU_ACTION_VIEW = By.xpath("//a[contains(text(),'view')]");
-        By VIEW_LABEL_STRING = By.xpath("//label[contains(.,'String')]");
-        By VIEW_LABEL_TEXT = By.xpath("//label[contains(.,'Text')]");
-        By VIEW_LABEL_INT = By.xpath("//label[contains(.,'Int')]");
-        By VIEW_LABEL_DECIMAL = By.xpath("//label[contains(.,'Decimal')]");
-        By VIEW_LABEL_DATE = By.xpath("//label[contains(.,'Date')]");
-        By VIEW_LABEL_DATETIME = By.xpath("//label[contains(.,'Datetime')]");
-        By VIEW_LABEL_USER = By.xpath("//label[contains(.,'User')]");
-        By VIEW_TITLE_TAG = By.xpath("//h4[contains(.,'Tag')]");
-        By VIEW_BUTTON_CLOSE = By.xpath("//button[contains(.,'clear')]");
 
         createNewEntity();
-        WebElement tag = getDriver().findElement(MENU_ENTITY_TAG);
-        scrollClick(getDriver(), tag);
-        getDriver().findElement(BUTTON_ACTION).click();
-        jsClick(getDriver(), getDriver().findElement(MENU_ACTION_VIEW));
+        getEntity(getDriver(), "Tag");
+        clickActionsView(getWait(),getDriver(),0);
+        List<WebElement> viewField =getDriver().findElements(By.xpath("//span[@class='pa-view-field']"));
 
-        Assert.assertTrue(isElementPresent(VIEW_LABEL_STRING));
-        Assert.assertTrue(isElementPresent(VIEW_LABEL_TEXT));
-        Assert.assertTrue(isElementPresent(VIEW_LABEL_INT));
-        Assert.assertTrue(isElementPresent(VIEW_LABEL_DECIMAL));
-        Assert.assertTrue(isElementPresent(VIEW_LABEL_DATE));
-        Assert.assertTrue(isElementPresent(VIEW_LABEL_DATETIME));
-        Assert.assertTrue(isElementPresent(VIEW_LABEL_USER));
-        Assert.assertTrue(isElementPresent(VIEW_TITLE_TAG));
-        Assert.assertTrue(isElementPresent(VIEW_BUTTON_CLOSE));
+        Assert.assertEquals(getActualValues(viewField), EXPECTED_RESULT);
     }
 }
