8d05e39fda28d00b802b8809e424ddd5e02817e5
mishinaolia <73864752+mishinaolia@users.noreply.github.com>
2021-06-15 12:49:10 -0700

    RF_Java | EntityReferenceValuesDraftTest > testCreateNewDraftRecords (#485)
    * RF_Java-EntityReferenceValuesDraftTest
    
    * Fix the modifiers for the locators.
    Change on the correct Asserts.

--- src/test/java/EntityReferenceValuesDraftTest.java
+++ src/test/java/EntityReferenceValuesDraftTest.java
@@ -3,37 +3,36 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import static utils.ProjectUtils.start;
 import static utils.TestUtils.scrollClick;
 
 public class EntityReferenceValuesDraftTest extends BaseTest {
-    private static final String Label_Input_Value = "label value";
-    private static final String Filter1_Input_Value = "Filter1 value";
-    private static final String Filter2_Input_Value = "Filter2 value";
 
-    private static final By Reference_Values_Menu = By.xpath("//p[contains(text(),'Reference values')]");
-    private static final By Create_New_Folder_Button = By.xpath("//i[contains(text(),'create_new_folder')]");
-    private static final By Label_Input = By.xpath("//input[@id='label']");
-    private static final By Filter1_Input = By.xpath("//input[@id='filter_1']");
-    private static final By Filter2_Input = By.xpath("//input[@id='filter_2']");
-    private static final By Safe_Draft_Button = By.xpath("//*[@id='pa-entity-form-draft-btn']");
-    private static final By Pencil_Icon = By.xpath("//i[@class='fa fa-pencil']");
+    private static final String LABEL_INPUT_VALUE = "label value";
+    private static final String FILTER_1_INPUT_VALUE = "Filter1 value";
+    private static final String FILTER_2_INPUT_VALUE = "Filter2 value";
+
+    private static final By REFERENCE_VALUES_MENU = By.xpath("//p[contains(text(),'Reference values')]");
+    private static final By CREATE_NEW_FOLDER_BUTTON = By.xpath("//i[contains(text(),'create_new_folder')]");
+    private static final By LABEL_INPUT = By.xpath("//input[@id='label']");
+    private static final By FILTER_1_INPUT = By.xpath("//input[@id='filter_1']");
+    private static final By FILTER_2_INPUT = By.xpath("//input[@id='filter_2']");
+    private static final By SAFE_DRAFT_BUTTON = By.xpath("//*[@id='pa-entity-form-draft-btn']");
 
     @Test
     public void testCreateNewDraftRecord() {
-        start(getDriver());
-        scrollClick(getDriver(), getDriver().findElement(Reference_Values_Menu));
 
-        getDriver().findElement(Create_New_Folder_Button).click();
-        getDriver().findElement(Label_Input).sendKeys(Label_Input_Value);
-        getDriver().findElement(Filter1_Input).sendKeys(Filter1_Input_Value);
-        getDriver().findElement(Filter2_Input).sendKeys(Filter2_Input_Value);
-        getDriver().findElement(Safe_Draft_Button).click();
-        getDriver().findElement(Pencil_Icon).isDisplayed();
+        scrollClick(getDriver(), getDriver().findElement(REFERENCE_VALUES_MENU));
+
+        getDriver().findElement(CREATE_NEW_FOLDER_BUTTON).click();
+        getDriver().findElement(LABEL_INPUT).sendKeys(LABEL_INPUT_VALUE);
+        getDriver().findElement(FILTER_1_INPUT).sendKeys(FILTER_1_INPUT_VALUE);
+        getDriver().findElement(FILTER_2_INPUT).sendKeys(FILTER_2_INPUT_VALUE);
+        getDriver().findElement(SAFE_DRAFT_BUTTON).click();
+
 
         WebElement Pencil_Icon = findElement(By.xpath("//i[@class='fa fa-pencil']"));
         Assert.assertEquals(Pencil_Icon.getAttribute("class"), "fa fa-pencil");
-        Assert.assertNotNull(getDriver().findElement(By.xpath("//table[@id='pa-all-entities-table']")));
+        Assert.assertNull(findElement(By.className("card-body")).getAttribute("value"));
     }
 }
