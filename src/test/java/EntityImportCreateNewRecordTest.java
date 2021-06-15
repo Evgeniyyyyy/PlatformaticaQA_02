9512472436a50660e13380429cd8e9918e443fff
Tatiana Chueva <70036086+ChuevaTatiana@users.noreply.github.com>
2021-06-15 09:52:16 -0700

    "Refactoring createNewRecord, add new testDeleteRecord(), create depends between tests" (#502)

--- src/test/java/EntityImportCreateNewRecordTest.java
+++ src/test/java/EntityImportCreateNewRecordTest.java
@@ -4,12 +4,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.ProjectUtils;
+import utils.TestUtils;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static utils.TestUtils.scrollClick;
+import static utils.ProjectUtils.clickSave;
+import static utils.TestUtils.*;
 
 public class EntityImportCreateNewRecordTest extends BaseTest {
 
@@ -51,10 +54,19 @@ public class EntityImportCreateNewRecordTest extends BaseTest {
         return getDriver().findElement(By.xpath("//tbody/tr/td[1]/i")).getAttribute("class");
     }
 
-    private void clickSaveButton() {
-        getDriver().findElement(By.id("pa-entity-form-save-btn")).click();
-        getWait().until(ExpectedConditions.elementToBeClickable(getDriver().findElement(
-                By.xpath("//i[text() = 'create_new_folder']"))));
+    private String getShowingOne() {
+        return getDriver().findElement(By.xpath("//span[@class = 'pagination-info']")).getText();
+    }
+
+    private void clickDropDownMenu() {
+        getDriver().findElement(By.xpath("//i[contains(., 'menu')]")).click();
+    }
+
+    private void clickDeleteMenu() {
+        clickDropDownMenu();
+        getWait().until(movingIsFinished(By.xpath("//a[contains (text(), 'delete')]"))).click();
+        getWait().until(
+                ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class = 'card-body ']")));
     }
 
     @Test
@@ -65,7 +77,7 @@ public class EntityImportCreateNewRecordTest extends BaseTest {
         clickImportMenu();
         clickCreateNewFolderButten();
         inputValue();
-        clickSaveButton();
+        jsClick(getDriver(), getDriver().findElement(By.id("pa-entity-form-save-btn")));
 
         List<WebElement> cells = getDriver().findElements(By.xpath("//td[@class= 'pa-list-table-th']"));
         List<String> actualValues = cells.stream().map(WebElement::getText).collect(Collectors.toList());
@@ -73,5 +85,39 @@ public class EntityImportCreateNewRecordTest extends BaseTest {
         Assert.assertEquals(actualValues, expectedValues);
         Assert.assertEquals(cells.size(), expectedValues.size());
         Assert.assertEquals(getAttributeClass(), "fa fa-check-square-o");
+    }
+
+    @Test(dependsOnMethods = "testCreateNewRecord")
+    public void testDeleteRecord() {
+        String expectedTextCardBodyBeforeDelete = "String\n" + "Text\n" + "Int\n" + "Decimal\n" + "Date\n" + "Datetime\n" +
+                "File\n" + "User\n" + "Actions\n" + "Hello\n" + "everyone\n" + "555\n" + "55.55\n" +
+                "03/06/2021\n" +
+                "12/06/2021 09:09:09\n" +
+                "apptester1@tester.test\n" +
+                "menu\n" +
+                "Showing 1 to 1 of 1 rows";
+
+        String expectedTextRecycleBinBeforeDelete = "delete_outline";
+        String expectedTextRecycleBinAfterDelete = "delete_outline\n" + "1";
+
+        clickImportMenu();
+
+        String textCardBodyBeforeDelete = getDriver().findElement(By.xpath("//div[@class= 'card-body ']")).getText();
+        String textRecycleBinBeforeDelete = getDriver().findElement(
+                By.xpath("//a[@href= 'index.php?action=recycle_bin']")).getText();
+
+        Assert.assertEquals(textCardBodyBeforeDelete, expectedTextCardBodyBeforeDelete);
+        Assert.assertEquals(textRecycleBinBeforeDelete, expectedTextRecycleBinBeforeDelete);
+
+        clickDeleteMenu();
+
+        String textCardBodyAfterDelete = getDriver().findElement(By.xpath("//div[@class = 'card-body ']")).getText();
+        String textRecycleBinAfterDelete = getDriver().findElement(
+                By.xpath("//a[@href='index.php?action=recycle_bin']")).getText();
+
+        Assert.assertTrue(textCardBodyAfterDelete.isBlank());
+        Assert.assertTrue(textCardBodyAfterDelete.isEmpty());
+        Assert.assertNotEquals(textRecycleBinBeforeDelete, textRecycleBinAfterDelete);
+        Assert.assertEquals(textRecycleBinAfterDelete, expectedTextRecycleBinAfterDelete);
     }
 }
