61d5b2f7023d12517752dcec3c09d000d3655583
MariaMoroz <82988399+MariaMoroz@users.noreply.github.com>
2021-06-15 11:27:24 -0400

    refactor EntityBoardDraftRecordTest (#508)
    * refactor EntityBoardDraftRecordTest

--- src/test/java/EntityBoardDraftRecordTest.java
+++ src/test/java/EntityBoardDraftRecordTest.java
@@ -1,13 +1,13 @@
 import base.BaseTest;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import utils.TestUtils;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import static utils.ProjectUtils.*;
@@ -15,143 +15,153 @@ import static utils.TestUtils.*;
 
 public class EntityBoardDraftRecordTest extends BaseTest {
 
-    private static final By INTEGER_FIELD  = By.id("int");
-    private static final By TEXT_FIELD  = By.id("text");
-    private static final By DECIMAL_FIELD  = By.id("decimal");
+    private static final By TEXT_FIELD = By.id("text");
+    private static final By INTEGER_FIELD = By.id("int");
+    private static final By DECIMAL_FIELD = By.id("decimal");
     private static final By ACTUAL_SEARCH_RECORD = By.xpath("//tbody/tr/td/a");
 
-    private static final String STRING_INPUT_VALUE = "Pending";
-    private static final String INT_INPUT_VALUE = "12345";
-    private static final String TEXT_INPUT_VALUE = "qwerty";
-    private static final String DECIMAL_INPUT_VALUE = "12.45";
-    private static final String NEW_DECIMAL_INPUT_VALUE = "0.22";
+    private static final String ENTITY_NAME = "Board";
+    private static final String STRING_INPUT_PENDING = "Pending";
+    private static final String STRING_INPUT_ONTRACK = "On track";
     private static final String USER_NAME = "tester10@tester.test";
-    private static final String EMPTY_FIELD = "";
 
-    private void clickListButton(WebDriver driver) {
-        driver.findElement(By.xpath(
+    private static final List<String> EXPECTED_CREATED_PENDING_RECORD = makeRandomData(STRING_INPUT_PENDING);
+    private static final List<String> EXPECTED_CREATED_ONTRACK_RECORD = makeRandomData(STRING_INPUT_ONTRACK);
+
+    private static List<String> makeRandomData(String dropDownItem) {
+        List<String> randomData = new ArrayList<>();
+
+        randomData.add(dropDownItem);
+        randomData.add(RandomStringUtils.randomAlphabetic(8));
+        randomData.add("" + RandomUtils.nextInt(0, 10000));
+        randomData.add(RandomUtils.nextInt(0, 10000) + "." + RandomStringUtils.randomNumeric(2));
+
+        randomData.add("");
+        randomData.add("");
+        randomData.add("");
+
+        randomData.add(USER_NAME);
+
+        return randomData;
+    }
+
+    private void clickListButton() {
+        getWait().until(ExpectedConditions.elementToBeClickable(By.xpath(
+                "//a[@href='index.php?action=action_list&list_type=table&entity_id=31']")));
+        findElement(By.xpath(
                 "//a[@href='index.php?action=action_list&list_type=table&entity_id=31']")).click();
-    }
-
-    private  void clickActionsView(WebDriver driver) {
-        driver.findElement(By.xpath("//button/i[text()='menu']")).click();
-        getWait().until(TestUtils.movingIsFinished(By.linkText("view"))).click();
-    }
-
-    private  void clickActionsEdit(WebDriver driver) {
-        driver.findElement(By.xpath("//button/i[text()='menu']")).click();
-        getWait().until(movingIsFinished(By.linkText("edit"))).click();
-    }
-
-    private  void clickActionsDelete(WebDriver driver) {
-        driver.findElement(By.xpath("//button/i[text()='menu']")).click();
-        getWait().until(movingIsFinished(By.linkText("delete"))).click();
-    }
-
-    private void fillForm() {
-        clickCreateRecord(getDriver());
-
-        sendKeysOneByOne(findElement(INTEGER_FIELD), INT_INPUT_VALUE);
-        sendKeysOneByOne(findElement(TEXT_FIELD), TEXT_INPUT_VALUE);
-        sendKeysOneByOne(findElement(DECIMAL_FIELD), DECIMAL_INPUT_VALUE);
-
-        findElement(By.xpath("//button[@data-id='string']")).click();
-        jsClick(getDriver(), findElement(By.xpath("//select/option[text()='Pending']")));
-        getWait().until(
-                ExpectedConditions.invisibilityOf(findElement(By.xpath("//div[@class='dropdown-menu ']"))));
-
-        scrollClick(getDriver(), findElement(By.xpath("//button[@data-id='user']")));
-        jsClick(getDriver(), findElement(By.xpath("//span[text()='tester10@tester.test']")));
-
-        clickSaveDraft(getDriver());
     }
 
     private void sendKeysOneByOne(WebElement element, String input) {
         char[] editKeys = input.toCharArray();
+        StringBuilder newString = new StringBuilder();
         for (char c : editKeys) {
+            newString.append(c);
             element.sendKeys(String.valueOf(c));
+            getWait().until(ExpectedConditions.attributeToBe(element,"value", String.valueOf(newString)));
         }
     }
 
-    private void editDraftRecord(){
-        clickActionsEdit(getDriver());
+    private void fillFormFields(List<String> data) {
+        sendKeysOneByOne(findElement(TEXT_FIELD), data.get(1));
+        getWait().until(ExpectedConditions.attributeToBe(findElement(TEXT_FIELD),"value", data.get(1)));
+        sendKeysOneByOne(findElement(INTEGER_FIELD), data.get(2));
+        getWait().until(ExpectedConditions.attributeToBe(findElement(INTEGER_FIELD),"value", data.get(2)));
+        sendKeysOneByOne(findElement(DECIMAL_FIELD), data.get(3));
+    }
 
-        findElement(DECIMAL_FIELD).click();
-        findElement(DECIMAL_FIELD).clear();
-        sendKeysOneByOne(findElement(DECIMAL_FIELD), NEW_DECIMAL_INPUT_VALUE);
+    private void fillDropDownFields(String dropDownItem) {
+        WebElement stringButton = findElement(By.xpath("//button[@data-id='string']"));
+        stringButton.click();
 
-        clickSaveDraft(getDriver());
+        jsClick(getDriver(),
+                findElement(By.xpath("//ul[@class='dropdown-menu inner show']//span[contains(.,'" + dropDownItem + "')]")));
+        getWait().until(
+                ExpectedConditions.invisibilityOf(findElement(By.xpath("//div[@class='dropdown-menu']"))));
+
+        Assert.assertEquals(stringButton.getAttribute("title"), dropDownItem);
+
+        scrollClick(getDriver(), findElement(By.xpath("//button[@data-id='user']")));
+        jsClick(getDriver(), findElement(By.xpath("//span[text()='tester10@tester.test']")));
+    }
+
+    private void checkRecordInViewMode(List<String> data) {
+        List<WebElement> actualRecords = findElements(By.xpath("//span [@class = 'pa-view-field']"));
+
+        for (int i = 0; i < actualRecords.size(); i++) {
+            Assert.assertEquals(actualRecords.get(i).getText(), data.get(i));
+        }
+        WebElement user = findElement(By.xpath("//div[@class = 'form-group']/p"));
+        Assert.assertEquals(user.getText(), data.get(7));
     }
 
     @Test
     public void testCreateDraftRecord() {
 
-        final List<String> expectedCreatedRecord = Arrays
-                .asList(STRING_INPUT_VALUE, TEXT_INPUT_VALUE, INT_INPUT_VALUE, DECIMAL_INPUT_VALUE,
-                        EMPTY_FIELD, EMPTY_FIELD, EMPTY_FIELD, USER_NAME);
+        getEntity(getDriver(), ENTITY_NAME);
 
-        getEntity(getDriver(),"Board");
+        clickCreateRecord(getDriver());
+        fillDropDownFields(STRING_INPUT_PENDING);
+        fillFormFields(EXPECTED_CREATED_PENDING_RECORD);
+        clickSaveDraft(getDriver());
 
-        fillForm();
-
-        clickListButton(getDriver());
+        clickListButton();
 
         WebElement icon = findElement(By.xpath("//tbody/tr/td/i"));
         Assert.assertEquals(icon.getAttribute("class"), "fa fa-pencil");
 
         List<WebElement> actualRecord = findElements(By.xpath("//td[@class='pa-list-table-th']"));
-        Assert.assertEquals(getActualValues(actualRecord), expectedCreatedRecord);
+        Assert.assertEquals(getActualValues(actualRecord), EXPECTED_CREATED_PENDING_RECORD);
     }
 
     @Test(dependsOnMethods = "testCreateDraftRecord")
     public void testViewDraftRecord() {
 
-        final List<String> expectedViewedRecord = Arrays
-                .asList(STRING_INPUT_VALUE, TEXT_INPUT_VALUE, INT_INPUT_VALUE, DECIMAL_INPUT_VALUE,
-                        EMPTY_FIELD, EMPTY_FIELD);
-
-        getEntity(getDriver(),"Board");
-        clickListButton(getDriver());
+        getEntity(getDriver(), ENTITY_NAME);
+        clickListButton();
 
         WebElement iconCheckBox = findElement(By.xpath("//tbody/tr/td/i"));
         Assert.assertEquals(iconCheckBox.getAttribute("class"), "fa fa-pencil");
 
-        clickActionsView(getDriver());
+        clickActionsView(getWait(), getDriver());
 
-        List<WebElement> actualRecord = findElements(By.xpath("//span[@class='pa-view-field']"));
-        WebElement actualUser = findElement(By.xpath("//div[@class='form-group']/p"));
-
-        Assert.assertEquals(getActualValues(actualRecord), expectedViewedRecord);
-        Assert.assertEquals(actualUser.getText(), USER_NAME);
+        checkRecordInViewMode(EXPECTED_CREATED_PENDING_RECORD);
     }
 
     @Test(dependsOnMethods = "testViewDraftRecord")
     public void testEditDraftRecord() {
 
-        final List<String> expectedEditedRecord = Arrays
-                .asList(STRING_INPUT_VALUE, TEXT_INPUT_VALUE, INT_INPUT_VALUE, NEW_DECIMAL_INPUT_VALUE,
-                        EMPTY_FIELD, EMPTY_FIELD, EMPTY_FIELD, USER_NAME);
-
-        getEntity(getDriver(),"Board");
-        clickListButton(getDriver());
+        getEntity(getDriver(), ENTITY_NAME);
+        clickListButton();
 
         List<WebElement> actualRecord = findElements(By.xpath("//tbody/tr"));
         Assert.assertEquals(actualRecord.size(), 1);
 
-        editDraftRecord();
+        clickActionsEdit(getWait(), getDriver());
+
+        findElement(TEXT_FIELD).click();
+        findElement(TEXT_FIELD).clear();
+        findElement(INTEGER_FIELD).click();
+        findElement(INTEGER_FIELD).clear();
+        findElement(DECIMAL_FIELD).click();
+        findElement(DECIMAL_FIELD).clear();
+
+        fillFormFields(EXPECTED_CREATED_PENDING_RECORD);
+
+        clickSaveDraft(getDriver());
 
         List<WebElement> actualEditedRecord = findElements(By.xpath("//td[@class='pa-list-table-th']"));
-        Assert.assertEquals(getActualValues(actualEditedRecord), expectedEditedRecord);
+        Assert.assertEquals(getActualValues(actualEditedRecord), EXPECTED_CREATED_PENDING_RECORD);
     }
 
     @Test(dependsOnMethods = "testEditDraftRecord")
     public void testDeleteDraftRecord() {
 
-        getEntity(getDriver(),"Board");
+        getEntity(getDriver(), ENTITY_NAME);
 
-        clickListButton(getDriver());
+        clickListButton();
 
-        clickActionsDelete(getDriver());
+        clickActionsDelete(getWait(), getDriver());
 
         String textCardBodyAfterDelete = findElement(By.xpath("//div[@class = 'card-body ']")).getText();
         Assert.assertTrue(textCardBodyAfterDelete.isEmpty());
@@ -168,10 +178,6 @@ public class EntityBoardDraftRecordTest extends BaseTest {
     @Test(dependsOnMethods = "testDeleteDraftRecord")
     public void testDeleteRecordFromRecycleBin() {
 
-        final List<String> expectedDelitedRecord = Arrays
-                .asList(STRING_INPUT_VALUE, TEXT_INPUT_VALUE, INT_INPUT_VALUE, NEW_DECIMAL_INPUT_VALUE,
-                        EMPTY_FIELD, EMPTY_FIELD);
-
         clickRecycleBin(getDriver());
 
         WebElement header = findElement(By.xpath("//a[@class='navbar-brand']"));
@@ -180,10 +186,7 @@ public class EntityBoardDraftRecordTest extends BaseTest {
         getWait().until(ExpectedConditions.elementToBeClickable(By
                 .xpath("//td[@class='pa-recycle-col']/a"))).click();
 
-        List<WebElement> actualRecord = findElements(By.xpath("//span[@class='pa-view-field']"));
-        WebElement actualUser = findElement(By.xpath("//div[@class='form-group']/p"));
-        Assert.assertEquals(getActualValues(actualRecord), expectedDelitedRecord);
-        Assert.assertEquals(actualUser.getText(), USER_NAME);
+        checkRecordInViewMode(EXPECTED_CREATED_PENDING_RECORD);
 
         findElement(By.xpath("//button[contains(.,'clear')]")).click();
 
@@ -194,40 +197,34 @@ public class EntityBoardDraftRecordTest extends BaseTest {
     }
 
     @Test
-    public void testSearchDraftRecord(){
+    public void testSearchDraftRecord() {
 
         final By searchField = By.xpath("//input[contains(@class, 'search-input')]");
-        final List<String> expectedFoundRecord = Arrays
-                .asList(STRING_INPUT_VALUE, "books", "52", "5.36",
-                        EMPTY_FIELD, EMPTY_FIELD);
 
-        final String searchTextValue = "books";
-
-        getEntity(getDriver(),"Board");
+        getEntity(getDriver(), ENTITY_NAME);
 
         clickCreateRecord(getDriver());
-
-        sendKeysOneByOne(findElement(INTEGER_FIELD), "52");
-        sendKeysOneByOne(findElement(TEXT_FIELD), "books");
-        sendKeysOneByOne(findElement(DECIMAL_FIELD), "5.36");
+        fillDropDownFields(STRING_INPUT_PENDING);
+        fillFormFields(EXPECTED_CREATED_PENDING_RECORD);
         clickSaveDraft(getDriver());
 
         clickCreateRecord(getDriver());
-
-        sendKeysOneByOne(findElement(INTEGER_FIELD), "199");
-        sendKeysOneByOne(findElement(TEXT_FIELD), "magazines");
-        sendKeysOneByOne(findElement(DECIMAL_FIELD), "1.78");
+        fillDropDownFields(STRING_INPUT_ONTRACK);
+        fillFormFields(EXPECTED_CREATED_ONTRACK_RECORD);
         clickSaveDraft(getDriver());
 
-        clickListButton(getDriver());
+        clickListButton();
 
         Assert.assertEquals(findElements(By.xpath("//tbody/tr")).size(), 2);
 
-        findElement(searchField).sendKeys(searchTextValue);
+        findElement(searchField).sendKeys(STRING_INPUT_PENDING);
 
         getWait().until(ExpectedConditions.textToBePresentInElementLocated(
                 By.xpath("//span[@class='pagination-info']"), "Showing 1 to 1 of 1 rows"));
 
-        Assert.assertEquals(getActualValues(findElements(ACTUAL_SEARCH_RECORD)), expectedFoundRecord);
+        for (int i = 0; i < findElements(ACTUAL_SEARCH_RECORD).size(); i++) {
+            Assert.assertEquals(findElements(ACTUAL_SEARCH_RECORD).get(i).getText(),
+                    EXPECTED_CREATED_PENDING_RECORD.get(i));
+        }
     }
 }
