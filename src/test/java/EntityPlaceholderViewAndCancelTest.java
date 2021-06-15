2a869a974f30aabbf666dc8cb74595ef83d258b7
Olga Kogut <olga.kohut.t@gmail.com>
2021-06-15 12:52:46 +0300

    Refactor: Entity>Placeholder>ViewAndCancelTest

--- src/test/java/EntityPlaceholderViewAndCancelTest.java
+++ src/test/java/EntityPlaceholderViewAndCancelTest.java
@@ -6,11 +6,12 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-import static utils.ProjectUtils.start;
+import static utils.ProjectUtils.getEntity;
 import static utils.TestUtils.*;
 
-public class EntityPlaceholderViewAndCancel extends BaseTest {
+public class EntityPlaceholderViewAndCancelTest extends BaseTest {
 
     private static final String STRING_VALUE = "String";
     private static final String TEXT_VALUE = "Text";
@@ -18,11 +19,12 @@ public class EntityPlaceholderViewAndCancel extends BaseTest {
     private static final String DECIMAL_VALUE = "34.35";
     private static final String DATE_VALUE = "30/05/2021";
     private static final String DATETIME_VALUE = "02/06/2021 10:10:10";
+    private static final List<String> expectedCreatedResult = List.of(
+            STRING_VALUE, TEXT_VALUE, INT_VALUE, DECIMAL_VALUE, DATE_VALUE,
+            DATETIME_VALUE, "", "", "tester68@tester.test");
 
-    public void createRecord() {
-        start(getDriver());
-        scrollClick(getDriver(), findElement(By.xpath("//p[contains(text(),'Placeholder')]")));
-
+    public void fillForm() {
+        getEntity(getDriver(),"Placeholder");
         getWait().until(ExpectedConditions.elementToBeClickable(By.xpath("//i[contains(text(),'create_new_folder')]"))).click();
 
         findElement(By.id("string")).sendKeys(STRING_VALUE);
@@ -38,47 +40,39 @@ public class EntityPlaceholderViewAndCancel extends BaseTest {
         findElement(By.xpath("//button[@data-id='user']")).click();
         jsClick(getDriver(), findElement(
                 By.xpath("//span[text()='tester68@tester.test']")));
+
+        getWait().until(ExpectedConditions.attributeToBe(findElement(By.id("datetime")), "value", DATETIME_VALUE));
     }
 
     @Test
     public void testCreatedAndCancel() {
-        final List<String> expectedResult = List
-                .of(STRING_VALUE, TEXT_VALUE, INT_VALUE, DECIMAL_VALUE, DATE_VALUE, DATETIME_VALUE,
-                        "", "", "tester68@tester.test");
-
-        createRecord();
-        getWait().until(ExpectedConditions.attributeToBe(findElement(By.id("datetime")), "value", DATETIME_VALUE));
-        scrollClick(getDriver(), findElement(By.xpath("//button[contains(text(), \"Cancel\")]")));
+        fillForm();
+        scrollClick(getDriver(), findElement(By.xpath("//button[contains(text(), 'Cancel')]")));
 
         Assert.assertEquals(findElement(By.className("card-body")).getText(), "");
-        Assert.assertEquals(findElement(By.className("card-body")).getSize().height, 30);
-        Assert.assertNull(findElement(By.xpath("//a[@href=\"index.php?action=recycle_bin\"]")).getAttribute("span"));
-
-        List<WebElement> resultList = findElements(By.xpath("//tbody/tr/td[@class = 'pa-list-table-th']"));
-        for (int i = 0; i < expectedResult.size(); i++) {
-            Assert.assertFalse(resultList.contains(expectedResult.get(i)));
-        }
+        Assert.assertNull(findElement(By.xpath("//a[@href='index.php?action=recycle_bin']")).getAttribute("span"));
     }
 
-    @Test
+    @Test(dependsOnMethods = "testCreatedAndCancel")
+    public void testCreateRecord() {
+        fillForm();
+        scrollClick(getDriver(), findElement(By.id("pa-entity-form-save-btn")));
+        getWait().until(ExpectedConditions.elementToBeClickable(By.xpath("//i[text()='menu']")));
+
+        List<WebElement> resultList = findElements(By.xpath("//tbody/tr/td[@class = 'pa-list-table-th']"));
+        Assert.assertEquals(resultList.stream().map(WebElement::getText).collect(Collectors.toList()), expectedCreatedResult);
+    }
+
+    @Test(dependsOnMethods = "testCreateRecord")
     public void testViewRecord() {
         final List<String> expectedResult = List
                 .of(STRING_VALUE, TEXT_VALUE, INT_VALUE, DECIMAL_VALUE, DATE_VALUE, DATETIME_VALUE);
 
-        createRecord();
-        getWait().until(ExpectedConditions.attributeToBe(findElement(By.id("datetime")), "value", DATETIME_VALUE));
-        scrollClick(getDriver(), findElement(By.id("pa-entity-form-save-btn")));
-
+        getEntity(getDriver(), "Placeholder");
         getWait().until(ExpectedConditions.elementToBeClickable(By.xpath("//i[text()='menu']"))).click();
-        scroll(getDriver(), findElement(By.xpath("//a[text()='view']")));
-        getWait().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='view']"))).click();
+        getWait().until(movingIsFinished(By.xpath("//a[contains(., 'view')]"))).click();
 
         List<WebElement> resultList = findElements(By.xpath("//span[@class='pa-view-field']"));
-        Assert.assertEquals(resultList.size(), expectedResult.size());
-        for (int i = 0; i < resultList.size(); i++) {
-            Assert.assertNotNull(resultList.get(i));
-            Assert.assertEquals(resultList.get(i).getText(), expectedResult.get(i));
-        }
-        getWait().until(ExpectedConditions.elementToBeClickable(By.xpath("//i[contains(text(), \"clear\")]"))).click();
+        Assert.assertEquals(resultList.stream().map(WebElement::getText).collect(Collectors.toList()), expectedResult);
     }
 }
