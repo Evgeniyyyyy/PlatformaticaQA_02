b31441f12301ef221f95e5c7de770bc6bbb923da
Olga V <61809448+010V@users.noreply.github.com>
2021-06-16 11:58:34 -0700

    Fixed error in code for EntityCalendarDeleteDraftRecordPermanentlyTest (#489)

--- src/test/java/EntityCalendarDeleteDraftRecordPermanentlyTest.java
+++ src/test/java/EntityCalendarDeleteDraftRecordPermanentlyTest.java
@@ -1,10 +1,8 @@
 import base.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import utils.ProjectUtils;
 import utils.TestUtils;
@@ -16,86 +14,42 @@ public class EntityCalendarDeleteDraftRecordPermanentlyTest extends BaseTest {
     String intNumbers = "28";
     String decimalNumbers = "20.21";
 
-    private void navigateToCalendarPage() {
-        TestUtils.scrollClick(getDriver(), By.xpath("//p[contains (text(), 'Calendar')]"));
-
-    }
-
-    private void navigateToRecycleBin() {
-        WebElement recycleBin = findElement(By.xpath("//a/i[contains (text(), 'delete_outline')]"));
-        recycleBin.click();
-    }
-
     private void createDraftCalendarRecord() {
-        WebElement inputFieldString = findElement(By.xpath("//input[@id='string']"));
+        WebElement inputFieldString = findElement(By.id("string"));
         inputFieldString.clear();
         inputFieldString.sendKeys(newEventTitle);
 
-        WebElement inputFieldText = findElement(By.xpath("//textarea[@id='text']"));
+        WebElement inputFieldText = findElement(By.id("text"));
         inputFieldText.clear();
         inputFieldText.sendKeys(newEventDescription);
 
-        findElement(By.xpath("//input[@id='int']")).sendKeys(intNumbers);
-        findElement(By.xpath("//input[@id='decimal']")).sendKeys(decimalNumbers);
+        findElement(By.id("int")).sendKeys(intNumbers);
+        findElement(By.id("decimal")).sendKeys(decimalNumbers);
+        findElement(By.id("date")).click();
+        findElement(By.id("datetime")).click();
 
-        WebElement inputFieldDate = findElement(By.xpath("//input[@id='date']"));
-        inputFieldDate.click();
-
-        WebElement inputFieldDateTime = findElement(By.xpath("//input[@id='datetime']"));
-        inputFieldDateTime.click();
-
-        By fieldUser = By.xpath("//button[@data-id = 'user']");
-        getDriver().findElement(fieldUser).click();
-
-        Actions actions = new Actions(getDriver());
-        WebElement userchoice = getDriver().findElement(By.xpath("//div[@class= \"inner show\"]"));
-        actions.moveToElement(userchoice);
-
-        TestUtils.scroll(getDriver(), getDriver().findElement(
-                By.xpath("//ul [@class = 'dropdown-menu inner show']/li/a/span[ text() = 'tester18@tester.test']")));
-
-        getWait().until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//ul [@class = 'dropdown-menu inner show']/li/a/span[ text() = 'tester18@tester.test']")))
-                .click();
-
-        WebElement saveDraftButton = findElement(By.id("pa-entity-form-draft-btn"));
-        saveDraftButton.click();
+        ProjectUtils.clickSave(getDriver());
     }
 
-    private void deleteDraftCalendarRecord() {
-
-        WebElement buttonActions = findElement(By.xpath("//button[@class = \"btn btn-round btn-sm btn-primary dropdown-toggle\"]"));
-        buttonActions.click();
-
-        WebElement buttonDraftToBeDeleted = getWait().until(ExpectedConditions.elementToBeClickable(findElement(By.xpath("//a[contains (text(), \"delete\")]"))));
-        buttonDraftToBeDeleted.click();
-    }
-
-    @Ignore
     @Test
-    public void EntityCalendarDeleteDraftTest() {
+    public void EntityCalendarDeleteDraftPermanentlyTest() {
 
-        ProjectUtils.start(getDriver());
+        TestUtils.scrollClick(getDriver(), By.xpath("//p[contains (text(), 'Calendar')]"));
 
-        navigateToCalendarPage();
         findElement(By.xpath("//div[@class='card-icon']")).click();
 
         createDraftCalendarRecord();
 
-        findElement(By.xpath("//a [ @href = \"index.php?action=action_list&list_type=table&entity_id=32\"]")).click();
+        findElement(By.xpath("//*[contains(@href,'table&entity_id=32')]")).click();
+        findElement(By.xpath("//*[contains(@class, 'btn btn-round')]")).click();
+        findElement(By.xpath("//*[@class='dropdown-menu dropdown-menu-right show']/li[3]/a")).click();
 
-        deleteDraftCalendarRecord();
+        findElement(By.xpath("//a/i[contains (text(), 'delete_outline')]")).click();
 
-        navigateToRecycleBin();
+        getWait().until(ExpectedConditions.elementToBeClickable(
+               findElement(By.xpath("//a[contains (text(), 'delete permanently')]")))).click();
 
-        WebElement draftToBeDeletedFromRecycleBin = getWait().until(ExpectedConditions
-                .elementToBeClickable(findElement(By.xpath("//a[contains (text(), \"delete permanently\")]"))));
-
-        draftToBeDeletedFromRecycleBin.click();
-
-        WebElement contentRecycleBinCard = findElement(By.xpath("//div[@class = 'card-body']"));
-        String actualTextAfterDraftDeleted  = contentRecycleBinCard.getText();
-
-        Assert.assertEquals(actualTextAfterDraftDeleted, "Good job with housekeeping! Recycle bin is currently empty!");
+        Assert.assertEquals(findElement(By.xpath("//div[@class = 'card-body']")).getText(),
+                "Good job with housekeeping! Recycle bin is currently empty!");
     }
 }
