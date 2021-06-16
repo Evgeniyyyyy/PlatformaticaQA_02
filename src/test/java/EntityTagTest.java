854745ebc97aec7b84ad816c5499c021f6847eff
ValeriiSarukhanov <82187165+ValeriiSarukhanov@users.noreply.github.com>
2021-06-16 15:49:18 -0400

    RF | EntityTagTest > testName (#537)
    * Refactor. Delete constants.
    
    * Refactor. Delete constants.

--- src/test/java/EntityTagTest.java
+++ src/test/java/EntityTagTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
-import utils.ProjectUtils;
 
 import static utils.ProjectUtils.*;
 
@@ -12,17 +11,8 @@ import java.util.*;
 
 public class EntityTagTest extends BaseTest{
 
-    private static final String STRING = "Hello world";
-    private static final String TEXT = "Be healthy";
-    private static final String INT = "123";
-    private static final String DECIMAL = "456.98";
-    private static final String EDIT_STRING = "Entity Tag";
-    private static final String EDIT_TEXT = "Edit a record";
-    private static final String EDIT_INT = "8900";
-    private static final String EDIT_DECIMAL = "284555.98";
-
-    private static final List<String> EXPECTED_RESULT = List.of(STRING, TEXT, INT, DECIMAL, "", "");
-    private static final List<String> EDIT_RESULT = List.of(EDIT_STRING, EDIT_TEXT, EDIT_INT, EDIT_DECIMAL, "", "");
+    private static final List<String> EXPECTED_RESULT = List.of("Hello world", "Be healthy", "123", "456.98", "", "");
+    private static final List<String> EDIT_RESULT = List.of("Entity Tag", "Edit a record", "8900", "284555.98", "", "");
 
     private static final By ICON = By.xpath("//tbody/tr/td/i");
     private static final By ACTUAL_RESULT = By.xpath("//tbody/tr/td/a");
@@ -37,25 +27,25 @@ public class EntityTagTest extends BaseTest{
         getEntity(getDriver(),"Tag");
         clickCreateRecord(getDriver());
 
-        findElement(By.id("string")).sendKeys(STRING);
-        findElement(By.id("text")).sendKeys(TEXT);
-        findElement(By.id("int")).sendKeys(INT);
-        findElement(By.id("decimal")).sendKeys(DECIMAL);
+        findElement(FILL_STRING).sendKeys(EXPECTED_RESULT.get(0));
+        findElement(FILL_TEXT).sendKeys(EXPECTED_RESULT.get(1));
+        findElement(FILL_INT).sendKeys(EXPECTED_RESULT.get(2));
+        findElement(FILL_DECIMAL).sendKeys(EXPECTED_RESULT.get(3));
     }
 
     private void editRecord() {
 
         findElement(FILL_STRING).clear();
-        findElement(FILL_STRING).sendKeys(EDIT_STRING);
+        findElement(FILL_STRING).sendKeys(EDIT_RESULT.get(0));
 
         findElement(FILL_TEXT).clear();
-        findElement(FILL_TEXT).sendKeys(EDIT_TEXT);
+        findElement(FILL_TEXT).sendKeys(EDIT_RESULT.get(1));
 
         findElement(FILL_INT).clear();
-        findElement(FILL_INT).sendKeys(EDIT_INT);
+        findElement(FILL_INT).sendKeys(EDIT_RESULT.get(2));
 
         findElement(FILL_DECIMAL).clear();
-        findElement(FILL_DECIMAL).sendKeys(EDIT_DECIMAL);
+        findElement(FILL_DECIMAL).sendKeys(EDIT_RESULT.get(3));
         clickSave(getDriver());
     }
 
