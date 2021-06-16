470a32d6fd7ef17d9ed5b515c83b8d9bfb682229
Vitaly Zverev <54600489+VitalyVZH@users.noreply.github.com>
2021-06-16 21:02:16 +0300

    Added Entity > Visibility Events: testEditCreatedRecord (#533)

--- src/test/java/EntityVisibilityEventsTest.java
+++ src/test/java/EntityVisibilityEventsTest.java
@@ -1,13 +1,18 @@
 import base.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static utils.ProjectUtils.start;
+import static constants.EntityAssignConstants.*;
+import static utils.ProjectUtils.*;
 import static utils.TestUtils.scrollClick;
 
 public class EntityVisibilityEventsTest extends BaseTest {
+
+    final static String NEW_RECORD = "New modified record";
+    final static String ENTITY_NAME = "Visibility events";
 
     private void clickVisabilityEventsMenu(){
         scrollClick(getDriver(), getDriver().findElement(By.xpath("//p[text() = ' Visibility events ']")));
@@ -25,14 +30,22 @@ public class EntityVisibilityEventsTest extends BaseTest {
         getDriver().findElement(By.xpath("//span[@class ='toggle']")).click();
         getWait().until(ExpectedConditions.presenceOfElementLocated(By.id("test_field")));
     }
+
     private void inputTextValue(String text) {
-        getDriver().findElement(By.id("test_field")).sendKeys(text);
+
+        WebElement inputText = findElement(By.id("test_field"));
+
+        if (inputText.getText() != null) {
+            inputText.click();
+            inputText.clear();
+        }
+        inputText.sendKeys(text);
     }
 
     private void clickSaveButton(){
         getDriver().findElement(By.id("pa-entity-form-save-btn")).click();
         getWait().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//i[text() = 'create_new_folder']")));
-        }
+    }
 
     private String infoText(){
        return getDriver().findElement(By.xpath("//span[@class='pagination-info']")).getText();
@@ -40,6 +53,10 @@ public class EntityVisibilityEventsTest extends BaseTest {
 
     private String getAttributeClass(){
         return getDriver().findElement(By.xpath("//tbody/tr/td[2]/i")).getAttribute("class");
+    }
+
+    private String getIconClass() {
+        return findElement(ASSIGN_GET_ICON).getAttribute("class");
     }
 
     private String getValue(){
@@ -62,5 +79,18 @@ public class EntityVisibilityEventsTest extends BaseTest {
         Assert.assertEquals(getAttributeClass(),"fa fa-check-square-o");
         Assert.assertEquals(getValue(),"My first test 2021");
 
+    }
+
+    @Test(dependsOnMethods = "testCreateNewRecord")
+    public void testEditCreatedRecord() {
+
+        getEntity(getDriver(), ENTITY_NAME);
+        clickActionsEdit(getWait(), getDriver());
+        inputTextValue(NEW_RECORD);
+        clickSave(getDriver());
+
+        Assert.assertEquals(infoText(), TEXT_MESSAGE_ONE);
+        Assert.assertEquals(getIconClass(), CLASS_ITEM_SAVE);
+        Assert.assertEquals(getValue(), NEW_RECORD);
     }
 }
