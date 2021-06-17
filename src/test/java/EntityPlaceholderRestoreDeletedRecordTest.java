5f701a3d74c81ffbbfd28d5f668da3216dfa7597
Galina Kukleva <82921262+GALINA-KUKLEVA@users.noreply.github.com>
2021-06-16 20:57:09 -0600

    PlaceholderRestoreDeletedRecordTest refactored (#546)

--- src/test/java/EntityPlaceholderRestoreDeletedRecordTest.java
+++ src/test/java/EntityPlaceholderRestoreDeletedRecordTest.java
@@ -11,74 +11,82 @@ import static utils.ProjectUtils.*;
 import static utils.TestUtils.scrollClick;
 
 public class EntityPlaceholderRestoreDeletedRecordTest extends BaseTest {
+
+    private static final String TR_XPATH = "//table/tbody/tr";
+    private static final String STRING = "Task";
+    private static final String TEXT = "English";
+    private static final String INT = "25";
+    private static final String DECIMAL = "25.13";
+    private static final String DATE = "";
+    private static final String DATETIME = "";
+    private static final String FILE = "";
+    private static final String FILE_IMAGE = "";
+    private static final String USER_ID = "31";
+    private static final String USER = "tester29@tester.test";
+
+    private static final List<String> EXPECTED_RESULT = List.of(STRING, TEXT, INT, DECIMAL, USER_ID);
+    private static final List<String> EXPECTED_RESULT_1 = List.of(STRING, TEXT, INT, DECIMAL, DATE, DATETIME, FILE, FILE_IMAGE, USER);
+
+    private List<String> getActualResult(List<WebElement> testData) {
+        List<String> actualResult = new ArrayList<>();
+        for (int i = 0; i <= testData.size() - 1; i++) {
+            actualResult.add(testData.get(i).getText());
+        }
+
+        return actualResult;
+    }
+
     @Test
     public void testDeleteRecord() {
-
-        List<String> expectedResult = List.of("Task", "English", "25", "25.13", "31");
 
         scrollClick(getDriver(), By.xpath("//p[contains (text(), 'Placeholder')]"));
         clickCreateRecord(getDriver());
 
-        findElement(By.id("string")).sendKeys("Task");
-        findElement(By.id("text")).sendKeys("English");
-        findElement(By.id("int")).sendKeys("25");
-        findElement(By.id("decimal")).sendKeys("25.13");
+        findElement(By.id("string")).sendKeys(STRING);
+        findElement(By.id("text")).sendKeys(TEXT);
+        findElement(By.id("int")).sendKeys(INT);
+        findElement(By.id("decimal")).sendKeys(DECIMAL);
         findElement(
-                By.xpath("//div [@ id = '_field_container-user']/div/select/option [text() = 'tester29@tester.test']"))
+                By.xpath("//div [@ id = '_field_container-user']/div/select/option [text() = '" + USER + "']"))
                 .click();
 
         clickSave(getDriver());
         clickActionsDelete(getWait(), getDriver());
         clickRecycleBin(getDriver());
 
-        List<WebElement> rows = findElements(By.xpath("//table/tbody/tr"));
+        List<WebElement> rows = findElements(By.xpath(TR_XPATH));
 
         Assert.assertEquals(rows.size(), 1);
 
         List<WebElement> testData = findElements(
-                By.xpath("// table/tbody/tr/td [@class = 'pa-recycle-col']/a/span/b"));
-        List<String> actualResult = new ArrayList<>();
-        for (int i = 0; i <= testData.size() - 1; i++) {
-            actualResult.add(testData.get(i).getText());
-        }
+                By.xpath(TR_XPATH + "/td [@class = 'pa-recycle-col']/a/span/b"));
 
-        Assert.assertEquals(actualResult, expectedResult);
+        Assert.assertEquals(getActualResult(testData), EXPECTED_RESULT);
     }
 
     @Test(dependsOnMethods = "testDeleteRecord")
     public void testRestoreDeletedRecord() {
 
-        List<String> expectedResult = List.of("Task", "English", "25", "25.13", "31");
-        List<String> expectedResult1 = List.of("Task", "English", "25", "25.13", "", "", "", "", "tester29@tester.test");
-
         clickRecycleBin(getDriver());
 
-        List<WebElement> rows = findElements(By.xpath("//table/tbody/tr"));
+        List<WebElement> rows = findElements(By.xpath(TR_XPATH));
 
         Assert.assertEquals(rows.size(), 1);
 
         List<WebElement> testData = findElements(
-                By.xpath("// table/tbody/tr/td [@class = 'pa-recycle-col']/a/span/b"));
-        List<String> actualResult = new ArrayList<>();
-        for (int i = 0; i <= testData.size() - 1; i++) {
-            actualResult.add(testData.get(i).getText());
-        }
+                By.xpath(TR_XPATH + "/td [@class = 'pa-recycle-col']/a/span/b"));
 
-        Assert.assertEquals(actualResult, expectedResult);
+        Assert.assertEquals(getActualResult(testData), EXPECTED_RESULT);
 
         findElement(
-                By.xpath("//tbody/tr/td [@ class = 'pa-recycle-control']/a[contains (text(), 'restore as draft')]"))
+                By.xpath(TR_XPATH + "/td [@class = 'pa-recycle-control']/a[contains (text(), 'restore as draft')]"))
                 .click();
         scrollClick(getDriver(), By.xpath("//p[contains (text(), 'Placeholder')]"));
 
         List<WebElement> testData1 = findElements(
-                By.xpath("//table/tbody/tr/td [@class = 'pa-list-table-th']"));
-        List<String> actualResult1 = new ArrayList<>();
-        for (int i = 0; i <= testData1.size() - 1; i++) {
-            actualResult1.add(testData1.get(i).getText());
-        }
+                By.xpath(TR_XPATH + "/td [@class = 'pa-list-table-th']"));
 
-        Assert.assertEquals(actualResult1, expectedResult1);
+        Assert.assertEquals(getActualResult(testData1), EXPECTED_RESULT_1);
 
         WebElement icon = findElement(By.xpath("//td [1]/i"));
 
