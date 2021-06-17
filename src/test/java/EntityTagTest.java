1b8a74880b18af64ea65c763ad4ff4a3d147d37a
ValeriiSarukhanov <82187165+ValeriiSarukhanov@users.noreply.github.com>
2021-06-17 18:26:40 -0400

    New random input method. (#562)

--- src/test/java/EntityTagTest.java
+++ src/test/java/EntityTagTest.java
@@ -1,50 +1,77 @@
 import base.BaseTest;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.TestUtils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import static utils.ProjectUtils.*;
 
 public class EntityTagTest extends BaseTest{
 
-    private static final List<String> EXPECTED_RESULT = List.of("Hello world", "Be healthy", "123", "456.98", "", "");
-    private static final List<String> EDIT_RESULT = List.of("Entity Tag", "Edit a record", "8900", "284555.98", "", "");
-
     private static final By ICON = By.xpath("//tbody/tr/td/i");
-    private static final By ACTUAL_RESULT = By.xpath("//tbody/tr/td/a");
-    private static final By DELETED_RECORD = By.cssSelector("span.pa-view-field");
-    private static final By FILL_STRING = By.id("string");
-    private static final By FILL_TEXT = By.id("text");
-    private static final By FILL_INT = By.id("int");
-    private static final By FILL_DECIMAL = By.id("decimal");
+    private static final By ACTUAL_RESULT = By.xpath("//td[@class='pa-list-table-th']");
+    private static final By FIELD_STRING = By.id("string");
+    private static final By FIELD_TEXT = By.id("text");
+    private static final By FIELD_INT = By.id("int");
+    private static final By FIELD_DECIMAL = By.id("decimal");
+    private static final By FIELD_DATE = By.id("date");
+    private static final By FIELD_DATE_TIME = By.id("datetime");
+
+    private static final List<By> ELEMENTS = List.of(
+            FIELD_STRING, FIELD_TEXT, FIELD_INT, FIELD_DECIMAL, FIELD_DATE, FIELD_DATE_TIME);
+
+    Random random = new Random();
+    Date date = new Date();
+
+    List <String> EXPECTED_RESULT = List.of(
+            RandomStringUtils.randomAlphabetic(3),
+            RandomStringUtils.randomAlphabetic(30),
+            String.valueOf(random.nextInt(10000)),
+            String.format("%.2f", random.nextFloat()),
+            new SimpleDateFormat("dd/MM/yyyy").format(date),
+            new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(date),
+            "", "tester26@tester.test");
+
+    List <String> EDIT_RESULT = List.of(
+            RandomStringUtils.randomAlphabetic(20),
+            RandomStringUtils.randomAlphabetic(15),
+            String.valueOf(random.nextInt(10000)),
+            String.format("%.2f", random.nextFloat()),
+            "15/11/0078", "17/06/0902 12:31:44",
+            "", "tester27@tester.test");
 
     private void fillForm() {
 
         getEntity(getDriver(),"Tag");
         clickCreateRecord(getDriver());
 
-        findElement(FILL_STRING).sendKeys(EXPECTED_RESULT.get(0));
-        findElement(FILL_TEXT).sendKeys(EXPECTED_RESULT.get(1));
-        findElement(FILL_INT).sendKeys(EXPECTED_RESULT.get(2));
-        findElement(FILL_DECIMAL).sendKeys(EXPECTED_RESULT.get(3));
+        for (int i = 0; i < ELEMENTS.size(); i++) {
+            findElement(ELEMENTS.get(i)).click();
+            findElement(ELEMENTS.get(i)).clear();
+            findElement(ELEMENTS.get(i)).sendKeys(EXPECTED_RESULT.get(i));
+        }
+
+        TestUtils.jsClick(getDriver(), findElement(By.xpath("//div[@class='filter-option-inner-inner']")));
+        TestUtils.jsClick(getDriver(), findElement(By.xpath("//span[text()='tester26@tester.test']")));
     }
 
-    private void editRecord() {
+    private void editForms() {
 
-        findElement(FILL_STRING).clear();
-        findElement(FILL_STRING).sendKeys(EDIT_RESULT.get(0));
+        for (int i = 0; i < ELEMENTS.size(); i++) {
+            findElement(ELEMENTS.get(i)).clear();
+            findElement(ELEMENTS.get(i)).sendKeys(EDIT_RESULT.get(i));
+        }
 
-        findElement(FILL_TEXT).clear();
-        findElement(FILL_TEXT).sendKeys(EDIT_RESULT.get(1));
+        TestUtils.jsClick(getDriver(), findElement(By.xpath("//div[@class='filter-option-inner-inner']")));
+        TestUtils.jsClick(getDriver(), findElement(By.xpath("//span[text()='tester27@tester.test']")));
 
-        findElement(FILL_INT).clear();
-        findElement(FILL_INT).sendKeys(EDIT_RESULT.get(2));
-
-        findElement(FILL_DECIMAL).clear();
-        findElement(FILL_DECIMAL).sendKeys(EDIT_RESULT.get(3));
         clickSave(getDriver());
     }
 
@@ -84,7 +111,7 @@ public class EntityTagTest extends BaseTest{
 
         getEntity(getDriver(), "Tag");
         clickActionsEdit(getWait(), getDriver());
-        editRecord();
+        editForms();
 
         Assert.assertEquals(getActualValues(findElements(ACTUAL_RESULT)), EDIT_RESULT);
     }
@@ -95,17 +122,23 @@ public class EntityTagTest extends BaseTest{
         getEntity(getDriver(), "Tag");
         clickActionsDelete(getWait(), getDriver());
         clickRecycleBin(getDriver());
-        findElement(ACTUAL_RESULT).click();
+        findElement(By.xpath("//tbody/tr/td/a")).click();
 
-        Assert.assertEquals(getActualValues(findElements(DELETED_RECORD)), EXPECTED_RESULT);
+        List<WebElement> deleteRow = getDriver().findElements(By.cssSelector("span.pa-view-field"));
+        for (int i = 0; i < deleteRow.size(); i++) {
+            Assert.assertEquals(deleteRow.get(i).getText(), EXPECTED_RESULT.get(i));
+        }
     }
 
     @Test(dependsOnMethods = "testCreateRecord")
     public void testViewRecord(){
-        getEntity(getDriver(), "Tag");
-        clickActionsView(getWait(),getDriver(),0);
-        List<WebElement> viewField =getDriver().findElements(By.xpath("//span[@class='pa-view-field']"));
 
-        Assert.assertEquals(getActualValues(viewField), EXPECTED_RESULT);
+        getEntity(getDriver(), "Tag");
+        clickActionsView(getWait(),getDriver());
+
+        List<WebElement> viewField = getDriver().findElements(By.xpath("//span[@class='pa-view-field']"));
+        for (int i = 0; i < viewField.size(); i++) {
+            Assert.assertEquals(viewField.get(i).getText(), EXPECTED_RESULT.get(i));
+        }
     }
 }
