508e69e944d2ed733626bab4814be7d76af98913
Galina Kukleva <82921262+GALINA-KUKLEVA@users.noreply.github.com>
2021-06-17 19:03:39 -0600

    RF_ EntityReferenceValues1Test > testReferenceValues (#543)
    * testReferenceValues() refactored
    
    * Update EntityReferenceValues1Test.java
    
    Extra space deleted
    
    * Update EntityReferenceValues1Test.java
    
    Space added for restart CI
    
    Co-authored-by: ArinaJur <66705803+ArinaJur@users.noreply.github.com>

--- src/test/java/EntityReferenceValues1Test.java
+++ src/test/java/EntityReferenceValues1Test.java
@@ -3,40 +3,34 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import utils.ProjectUtils;
 import utils.TestUtils;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EntityReferenceValues1Test extends BaseTest {
+
+    private static final String LABEL = "Label";
+    private static final String FILTER_1 = "Area";
+    private static final String FILTER_2 = "Season";
+
+    private static final List<String> EXPECTED_VALUES = List.of(LABEL, FILTER_1, FILTER_2);
+
     @Test
     public void testReferenceValues() {
-
-        List<String> expectedValues = List.of("Label", "Area", "Season");
-
-        ProjectUtils.start(getDriver());
+        
         TestUtils.scrollClick(getDriver(), By.xpath("// p[contains (text(), 'Reference values')] "));
+
         getDriver().findElement(By.xpath("// i[contains (text(), 'create_new_folder')]")).click();
-        getDriver().findElement(By.id("label")).sendKeys("Label");
-        getDriver().findElement(By.id("filter_1")).sendKeys("Area");
-        getDriver().findElement(By.id("filter_2")).sendKeys("Season");
+        getDriver().findElement(By.id("label")).sendKeys(LABEL);
+        getDriver().findElement(By.id("filter_1")).sendKeys(FILTER_1);
+        getDriver().findElement(By.id("filter_2")).sendKeys(FILTER_2);
         getDriver().findElement(By.id("pa-entity-form-save-btn")).click();
 
-        Assert.assertEquals(getDriver().findElement(
-                By.xpath("//span [@class = 'pagination-info']")).getText(), "Showing 1 to 1 of 1 rows");
-
         List<WebElement> cells = getDriver().findElements(By.xpath("//td [@class = 'pa-list-table-th']"));
+        WebElement firstColumn = getDriver().findElement(By.xpath("// td/i"));
 
-        List<String> actualResult = new ArrayList<>();
-
-        for (WebElement cell : cells) {
-            actualResult.add(cell.getText());
-        }
-
-        Assert.assertEquals(actualResult, expectedValues);
-
-        Assert.assertEquals((getDriver().findElement(
-                By.xpath("// td/i")).getAttribute("class")), "fa fa-check-square-o");
+        Assert.assertEquals(cells.stream().map(WebElement::getText).collect(Collectors.toList()), EXPECTED_VALUES);
+        Assert.assertEquals(firstColumn.getAttribute("class"), "fa fa-check-square-o");
     }
 }
