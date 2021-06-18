fa91938e0ff54b7bb9f0e2810d9d22bf29ca3bbe
Andrey Teterin <robinhood.ex@gmail.com>
2021-06-17 21:40:04 -0400

    AT_069_Java| Entity > Arithmetic Function: Create a new record and save it. (#503)
    * Add testCreateRecord

--- src/test/java/EntityArithmeticFunctionTest.java
+++ src/test/java/EntityArithmeticFunctionTest.java
@@ -1,53 +1,18 @@
 import base.BaseTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import utils.ProjectUtils;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import utils.TestUtils;
-
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EntityArithmeticFunctionTest extends BaseTest {
 
     private static final By F1_FIELD = By.id("f1");
     private static final By F2_FIELD = By.id("f2");
     private static final By DIV_FIELD = By.id("div");
-
-
-    @Test
-    public void testRecordSaveDraft(){
-
-        final Integer F1 = 10;
-        final Integer F2 = 2;
-        final Integer DIV = F1 / F2;
-        final List<Integer> expectedList = List.of(F1, F2, F1 + F2, F1 - F2, F1 * F2, DIV);
-
-        ProjectUtils.start(getDriver());
-
-        TestUtils.scrollClick(getDriver(),
-                findElement(By.xpath("//p[contains(text(),'Arithmetic Function')]")));
-
-        ProjectUtils.clickCreateRecord(getDriver());
-
-        findElement(F1_FIELD).sendKeys(F1.toString());
-        findElement(F2_FIELD).sendKeys(F2.toString());
-
-        getWait().until(ExpectedConditions.attributeToBe(DIV_FIELD,"value", DIV.toString()));
-        ProjectUtils.clickSaveDraft(getDriver());
-
-        WebElement icon = findElement(By.xpath("//tbody/tr/td[1]/i"));
-        List<WebElement> columnList = findElements(By.xpath("//tbody/tr/td[@class = 'pa-list-table-th']"));
-
-        Assert.assertEquals(icon.getAttribute("class"), "fa fa-pencil");
-        Assert.assertEquals(columnList.size(), expectedList.size());
-        for (int i = 0; i < expectedList.size(); i++) {
-            Assert.assertEquals(columnList.get(i).getText(), expectedList.get(i).toString());
-        }
-    }
 
     private void sendKeysSlow(WebElement element, String text) {
         if (!element.getAttribute("value").isEmpty()) {
@@ -61,7 +26,7 @@ public class EntityArithmeticFunctionTest extends BaseTest {
                 Thread.sleep(200);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-                }
+            }
 
             newValue += ch;
             getWait().until(ExpectedConditions.attributeToBe(element, "value", newValue));
@@ -77,38 +42,68 @@ public class EntityArithmeticFunctionTest extends BaseTest {
         sendKeysSlow(f2, inputField2.toString());
 
         getWait().until(ExpectedConditions.attributeToBe(DIV_FIELD, "value", expectedValueDIV));
-        ProjectUtils.clickSave(getDriver());
     }
 
-    private void createRecord() {
-        TestUtils.scrollClick(getDriver(),
-                By.xpath("//p[text()=' Arithmetic Function ']/parent::a/parent::li"));
+    @Test
+    public void testCreateRecord(){
+        final Integer F1 = 20;
+        final Integer F2 = 5;
+        final List<Integer> expectedValues = List.of(F1, F2, F1 + F2, F1 - F2, F1 * F2, F1 / F2);
+
+        ProjectUtils.getEntity(getDriver(),"Arithmetic Function");
         ProjectUtils.clickCreateRecord(getDriver());
 
-        fillForm(20, 10);
+        fillForm(F1, F2);
+        ProjectUtils.clickSave(getDriver());
+
+        List<WebElement> actualValues = findElements(By.xpath("//tbody/tr/td[@class = 'pa-list-table-th']"));
+
+        Assert.assertTrue(getDriver().findElement(By.xpath("//tbody/tr/td[1]/i")).getAttribute("class").contains("fa-check-square-o"));
+        Assert.assertEquals(
+                actualValues.stream().map(WebElement::getText).collect(Collectors.toList()),
+                expectedValues.stream().map(String::valueOf).collect(Collectors.toList()));
     }
 
-    @Ignore
     @Test
+    public void testRecordSaveDraft(){
+        final Integer F1 = 10;
+        final Integer F2 = 2;
+        final Integer DIV = F1 / F2;
+        final List<Integer> expectedValues = List.of(F1, F2, F1 + F2, F1 - F2, F1 * F2, DIV);
+
+        ProjectUtils.getEntity(getDriver(),"Arithmetic Function");
+        ProjectUtils.clickCreateRecord(getDriver());
+
+        fillForm(F1, F2);
+        ProjectUtils.clickSaveDraft(getDriver());
+
+        List<WebElement> actualValues = findElements(By.xpath("//tbody/tr/td[@class = 'pa-list-table-th']"));
+
+        Assert.assertTrue(getDriver().findElement(By.xpath("//tbody/tr/td[1]/i")).getAttribute("class").contains("fa fa-pencil"));
+        Assert.assertEquals(
+                actualValues.stream().map(WebElement::getText).collect(Collectors.toList()),
+                expectedValues.stream().map(String::valueOf).collect(Collectors.toList()));
+    }
+
+    @Test(dependsOnMethods = "testCreateRecord")
     public void testEditRecord() {
         final Integer F1 = 40;
         final Integer F2 = 10;
-        final List<Integer> expectedList = List.of(F1, F2, F1 + F2, F1 - F2, F1 * F2, F1 / F2);
+        final List<Integer> expectedValues = List.of(F1, F2, F1 + F2, F1 - F2, F1 * F2, F1 / F2);
 
-        ProjectUtils.start(getDriver());
+        ProjectUtils.getEntity(getDriver(),"Arithmetic Function");
+        ProjectUtils.clickActionsEdit(getWait(), getDriver());
 
-        createRecord();
-
-        findElement(By.xpath("//div[@class='dropdown pull-left']")).click();
-        getWait().until(TestUtils.movingIsFinished(By.xpath("//a[text()='edit']"))).click();
+        Assert.assertNotNull(findElement(F1_FIELD).getAttribute("value"));
+        Assert.assertNotNull(findElement(F2_FIELD).getAttribute("value"));
 
         fillForm(F1, F2);
+        ProjectUtils.clickSave(getDriver());
 
-        List<WebElement> columnList = findElements(By.xpath("//tbody/tr/td[@class = 'pa-list-table-th']"));
+        List<WebElement> actualValues = findElements(By.xpath("//tbody/tr/td[@class = 'pa-list-table-th']"));
 
-        Assert.assertEquals(columnList.size(), expectedList.size());
-        for (int i = 0; i < expectedList.size(); i++) {
-            Assert.assertEquals(columnList.get(i).getText(), expectedList.get(i).toString());
-        }
+        Assert.assertEquals(
+                actualValues.stream().map(WebElement::getText).collect(Collectors.toList()),
+                expectedValues.stream().map(String::valueOf).collect(Collectors.toList()));
     }
 }
