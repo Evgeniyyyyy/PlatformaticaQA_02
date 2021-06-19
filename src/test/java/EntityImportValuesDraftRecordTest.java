3ddf635d2262fb7959ed7719482331ed3856c467
Olga Kovtun <83074160+olhakovtun@users.noreply.github.com>
2021-06-19 21:17:35 +0200

    testEditDraftRecord() created, method editForm() added (#579)

--- src/test/java/EntityImportValuesDraftRecordTest.java
+++ src/test/java/EntityImportValuesDraftRecordTest.java
@@ -28,13 +28,21 @@ public class EntityImportValuesDraftRecordTest extends BaseTest {
     private static final String USERNAME_INPUT = "apptester1@tester.test";
     private static final String FILE_INPUT = "";
     private static final String IMPORT_VALUES = "Import values";
+    private static final String STRING_INPUT2 = "New string";
+    private static final String TEXT_INPUT2 = "New text.";
+    private static final String INT_INPUT2 = "12";
+    private static final String DECIMAL_INPUT2 = "0.20";
+    private static final String DATE_INPUT2 = "18/07/2021";
+    private static final String DATETIME_INPUT2 = "18/07/2021 17:07:07";
 
-    private final static List<String> EXPECTED_VALUES = Arrays.asList(
-            STRING_INPUT, TEXT_INPUT, INT_INPUT, DECIMAL_INPUT,
-            DATE_INPUT, DATETIME_INPUT, FILE_INPUT, USERNAME_INPUT);
+    private final static List<String> EXPECTED_VALUES = Arrays.asList(STRING_INPUT, TEXT_INPUT,
+            INT_INPUT, DECIMAL_INPUT, DATE_INPUT, DATETIME_INPUT, FILE_INPUT, USERNAME_INPUT);
 
-    private final static List<String> EXPECTED_RECORDS = Arrays.asList(STRING_INPUT, TEXT_INPUT, INT_INPUT,
-            DECIMAL_INPUT, DATE_INPUT, DATETIME_INPUT);
+    private final static List<String> EXPECTED_RECORDS = Arrays.asList(STRING_INPUT, TEXT_INPUT,
+            INT_INPUT, DECIMAL_INPUT, DATE_INPUT, DATETIME_INPUT);
+
+    private final static List<String> EXPECTED_VALUES2 = Arrays.asList(STRING_INPUT2, TEXT_INPUT2,
+            INT_INPUT2, DECIMAL_INPUT2, DATE_INPUT2, DATETIME_INPUT2, FILE_INPUT, USERNAME_INPUT);
 
     private void fillForm() {
         findElement(STRING_FIELD).sendKeys(STRING_INPUT);
@@ -51,6 +59,35 @@ public class EntityImportValuesDraftRecordTest extends BaseTest {
         dateTime.click();
         dateTime.clear();
         dateTime.sendKeys(DATETIME_INPUT);
+    }
+
+    private void editForm() {
+        WebElement string = findElement(STRING_FIELD);
+        string.clear();
+
+        WebElement text = findElement(TEXT_FIELD);
+        text.clear();
+
+        WebElement intField = findElement(INT_FIELD);
+        intField.clear();
+
+        WebElement decimal = findElement(DECIMAL_FIELD);
+        decimal.clear();
+
+        WebElement date = findElement(DATE_FIELD);
+        date.click();
+        date.clear();
+        date.sendKeys(DATE_INPUT2);
+
+        WebElement dateTime = findElement(DATETIME_FIELD);
+        dateTime.click();
+        dateTime.clear();
+        dateTime.sendKeys(DATETIME_INPUT2);
+
+        string.sendKeys(STRING_INPUT2);
+        text.sendKeys(TEXT_INPUT2);
+        intField.sendKeys(INT_INPUT2);
+        decimal.sendKeys(DECIMAL_INPUT2);
     }
 
     @Test
@@ -104,5 +141,25 @@ public class EntityImportValuesDraftRecordTest extends BaseTest {
         }
 
         Assert.assertEquals(actualValues, EXPECTED_RECORDS);
+    }
+
+    @Test(dependsOnMethods = "testViewDraftRecord")
+    public void testEditDraftRecord() {
+        getEntity(getDriver(), IMPORT_VALUES);
+        clickActionsEdit(getWait(), getDriver());
+        editForm();
+        clickSaveDraft(getDriver());
+
+        List<WebElement> tds = findElements(By.xpath("//tbody/tr/td[@class = 'pa-list-table-th']"));
+
+        WebElement pencilIcon = getDriver().findElement(By.xpath("//tbody/tr/td[1]/i"));
+
+        List<String> actualValues = new ArrayList<>();
+        for (WebElement td : tds) {
+            actualValues.add(td.getText());
+        }
+
+        Assert.assertEquals(pencilIcon.getAttribute("class"), "fa fa-pencil");
+        Assert.assertEquals(actualValues, EXPECTED_VALUES2);
     }
 }
