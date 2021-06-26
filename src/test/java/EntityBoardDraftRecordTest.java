9e9849782b6cd96f3f1e82fbd898acd2b6c462ca
MariaMoroz <82988399+MariaMoroz@users.noreply.github.com>
2021-06-26 16:18:20 -0400

    refactoring EntityBoardDraftRecordTest, use common methods getUser(),getRecordInViewMode() (#639)

--- src/test/java/EntityBoardDraftRecordTest.java
+++ src/test/java/EntityBoardDraftRecordTest.java
@@ -11,15 +11,13 @@ import org.testng.annotations.Test;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static utils.ProjectUtils.getIntRandom;
-import static utils.ProjectUtils.getTextRandom;
+import static utils.ProjectUtils.*;
 
 public class EntityBoardDraftRecordTest extends BaseTest {
 
     private static final String STRING_INPUT_PENDING = "Pending";
     private static final String STRING_INPUT_ONTRACK = "On track";
-    private static final String STRING_INPUT_DONE = "Done";
-    private static final String USER_NAME = "tester" + new Random().nextInt(299) + "@tester.test";
+    private static final String USER_NAME = getUser();
     private static final String DRAFT_RECORD_ICON_CLASS_NAME = "fa fa-pencil";
     private static final String NON_DRAFT_RECORD_ICON_CLASS_NAME = "fa fa-check-square-o";
     private static final String PAGINATION_INFO_STR_1_OF_1 = "Showing 1 to 1 of 1 rows";
@@ -32,9 +30,6 @@ public class EntityBoardDraftRecordTest extends BaseTest {
     private static final String TEXT_VALUE_ONTRACK = getTextRandom(8);
     private static final String INT_VALUE_ONTRACK = getIntRandom();
     private static final String DECIMAL_VALUE_ONTRACK = getRandomDecimalValue();
-    private static final String TEXT_VALUE_DONE = getTextRandom(8);
-    private static final String INT_VALUE_DONE = getIntRandom();
-    private static final String DECIMAL_VALUE_DONE = getRandomDecimalValue();
     private static final String EDIT_TEXT_VALUE = "QWERTY";
     private static final String EDIT_INT_VALUE = "12345";
     private static final String EDIT_DECIMAL_VALUE = "33.55";
@@ -46,13 +41,6 @@ public class EntityBoardDraftRecordTest extends BaseTest {
             DECIMAL_VALUE_PENDING,
             "", "", "", USER_NAME);
 
-    private static final List<String> EXPECTED_VIEW_PENDING_RECORD = List.of(
-            STRING_INPUT_PENDING,
-            TEXT_VALUE_PENDING,
-            INT_VALUE_PENDING,
-            DECIMAL_VALUE_PENDING,
-            "", "");
-
     private static final List<String> EXPECTED_EDITED_RECORD = List.of(
             STRING_INPUT_ONTRACK,
             EDIT_TEXT_VALUE,
@@ -60,25 +48,11 @@ public class EntityBoardDraftRecordTest extends BaseTest {
             EDIT_DECIMAL_VALUE,
             "", "", "", USER_NAME);
 
-    private static final List<String> EXPECTED_EDITED_VIEWMODE_RECORD = List.of(
-            STRING_INPUT_ONTRACK,
-            EDIT_TEXT_VALUE,
-            EDIT_INT_VALUE,
-            EDIT_DECIMAL_VALUE,
-            "", "");
-
     private static final List<String> EXPECTED_CREATED_ONTRACK_RECORD = List.of(
             STRING_INPUT_ONTRACK,
             TEXT_VALUE_ONTRACK,
             INT_VALUE_ONTRACK,
             DECIMAL_VALUE_ONTRACK,
-            "", "", "", USER_NAME);
-
-    private static final List<String> EXPECTED_CREATED_DONE_RECORD = List.of(
-            STRING_INPUT_DONE,
-            TEXT_VALUE_DONE,
-            INT_VALUE_DONE,
-            DECIMAL_VALUE_DONE,
             "", "", "", USER_NAME);
 
     private static final List<List<String>> ALL_RECORDS_TABLE = new ArrayList<>(List.of(EXPECTED_CREATED_PENDING_RECORD,
@@ -91,7 +65,7 @@ public class EntityBoardDraftRecordTest extends BaseTest {
     BoardEditPage boardEditPage;
     BoardListPage boardListPage;
 
-    private BoardEditPage fillNewRecordFields(List<String> list) {
+    private BoardEditPage fillFormNewRecord(List<String> list) {
         return new MainPage(getDriver())
                 .clickBoardMenu()
                 .clickNewButton()
@@ -111,9 +85,7 @@ public class EntityBoardDraftRecordTest extends BaseTest {
                 .clickListButton()
                 .clickActions()
                 .clickActionsEdit()
-                .clearText()
-                .clearInt()
-                .clearDecimal()
+                .clearFields()
                 .fillFields(list);
     }
 
@@ -130,7 +102,7 @@ public class EntityBoardDraftRecordTest extends BaseTest {
     @Test
     public void testCreateDraftRecord() {
 
-        boardEditPage = fillNewRecordFields(EXPECTED_CREATED_PENDING_RECORD);
+        boardEditPage = fillFormNewRecord(EXPECTED_CREATED_PENDING_RECORD);
         boardEditPage.clickSaveDraft();
 
         boardListPage = goToBoardListPage();
@@ -149,8 +121,7 @@ public class EntityBoardDraftRecordTest extends BaseTest {
                 .clickActions()
                 .clickActionsView();
 
-        Assert.assertEquals(boardViewPage.getActualRecordInViewMode(), EXPECTED_VIEW_PENDING_RECORD);
-        Assert.assertEquals(boardViewPage.getActualUserName(), USER_NAME);
+        Assert.assertEquals(boardViewPage.getRecordInViewMode(), EXPECTED_CREATED_PENDING_RECORD);
     }
 
     @Test(dependsOnMethods = "testView")
@@ -212,8 +183,7 @@ public class EntityBoardDraftRecordTest extends BaseTest {
                 .clickRecycleBin()
                 .clickDeletedRecord(BoardViewPage::new, viewPage -> {
 
-                    Assert.assertEquals(viewPage.getActualRecordInViewMode(), EXPECTED_EDITED_VIEWMODE_RECORD);
-                    Assert.assertEquals(viewPage.getActualUserName(), USER_NAME);
+                    Assert.assertEquals(viewPage.getRecordInViewMode(), EXPECTED_EDITED_RECORD);
 
                 }).clickDeletedRecordPermanently();
 
@@ -225,13 +195,14 @@ public class EntityBoardDraftRecordTest extends BaseTest {
 
         ALL_RECORDS_TABLE.sort(new CompareByText());
 
-        List<String> expectedSortedRecords = ALL_RECORDS_TABLE.stream()
+        List<String> expectedSortedRecords = ALL_RECORDS_TABLE
+                .stream()
                 .flatMap(Collection::stream)
                 .collect(Collectors.toList());
 
-        boardEditPage = fillNewRecordFields(EXPECTED_CREATED_PENDING_RECORD);
+        boardEditPage = fillFormNewRecord(EXPECTED_CREATED_PENDING_RECORD);
         boardEditPage.clickSaveDraft();
-        boardEditPage = fillNewRecordFields(EXPECTED_CREATED_ONTRACK_RECORD);
+        boardEditPage = fillFormNewRecord(EXPECTED_CREATED_ONTRACK_RECORD);
         boardEditPage.clickSaveDraft();
 
         boardListPage = goToBoardListPage();
@@ -264,7 +235,7 @@ public class EntityBoardDraftRecordTest extends BaseTest {
 
         String textCardBodyBeforeCancel = boardPage.getTextCardBody();
 
-        boardEditPage = fillNewRecordFields(EXPECTED_CREATED_PENDING_RECORD);
+        boardEditPage = fillFormNewRecord(EXPECTED_CREATED_PENDING_RECORD);
         boardEditPage
                 .clickCancel();
 
@@ -275,7 +246,7 @@ public class EntityBoardDraftRecordTest extends BaseTest {
     @Test
     public void testCreateRecord() {
 
-        boardEditPage = fillNewRecordFields(EXPECTED_CREATED_PENDING_RECORD);
+        boardEditPage = fillFormNewRecord(EXPECTED_CREATED_PENDING_RECORD);
         boardEditPage.clickSave();
 
         boardListPage = goToBoardListPage();
@@ -314,13 +285,14 @@ public class EntityBoardDraftRecordTest extends BaseTest {
 
         ALL_RECORDS_TABLE.sort(new CompareByText());
 
-        List<String> expectedSortedRecords = ALL_RECORDS_TABLE.stream()
+        List<String> expectedSortedRecords = ALL_RECORDS_TABLE
+                .stream()
                 .flatMap(Collection::stream)
                 .collect(Collectors.toList());
 
-        boardEditPage = fillNewRecordFields(EXPECTED_CREATED_PENDING_RECORD);
+        boardEditPage = fillFormNewRecord(EXPECTED_CREATED_PENDING_RECORD);
         boardEditPage.clickSave();
-        boardEditPage = fillNewRecordFields(EXPECTED_CREATED_ONTRACK_RECORD);
+        boardEditPage = fillFormNewRecord(EXPECTED_CREATED_ONTRACK_RECORD);
         boardEditPage.clickSave();
 
         boardListPage = goToBoardListPage();
--- src/test/java/model/BoardViewPage.java
+++ src/test/java/model/BoardViewPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class BoardViewPage extends BaseViewPage<BoardListPage> {
@@ -15,21 +14,6 @@ public class BoardViewPage extends BaseViewPage<BoardListPage> {
 
     @FindBy(xpath = "//span [@class = 'pa-view-field']")
     private List<WebElement> viewModeRecord;
-
-    @FindBy(xpath = "//div[@class = 'form-group']/p")
-    private WebElement user;
-
-    public List<String> getActualRecordInViewMode(){
-        List<String> listValues = new ArrayList<>();
-        for (WebElement element : viewModeRecord) {
-            listValues.add(element.getText());
-        }
-        return listValues;
-    }
-
-    public String getActualUserName(){
-        return user.getText();
-    }
 
     @Override
     protected BoardListPage createMasterPage(){
