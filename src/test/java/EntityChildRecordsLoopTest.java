0ce84464165319ced873cdcdbbb064825743d421
MariaMoroz <82988399+MariaMoroz@users.noreply.github.com>
2021-07-05 15:07:40 -0400

    RF | EntityChildRecordsLoopTest> testCreateEmbedRecord() (#710)
    * refactor testCreateEmbedRecord()
    
    * rename test

--- src/test/java/EntityChildRecordsLoopTest.java
+++ src/test/java/EntityChildRecordsLoopTest.java
@@ -1,5 +1,5 @@
 import base.BaseTest;
-import model.ChildRecordsLoopEditPage;
+
 import model.ChildRecordsLoopPage;
 import model.MainPage;
 
@@ -22,7 +22,7 @@ public class EntityChildRecordsLoopTest extends BaseTest {
     private static final String VALUE_ITEM_FIELD1 = getTextRandom(5);
     private static final String VALUE_ITEM_FIELD2 = getTextRandom(5);
     private static final String VALUE_ITEM_FIELD3 = getTextRandom(5);
-    private static final int AMOUNT_OF_ROWS_IN_TABLE = 3;
+
     private static final String PAGINATION_INFO_STR_1_OF_1 = "Showing 1 to 1 of 1 rows";
 
     private static final List<Integer> LIST_VALUES_AMOUNT_FIELD = Arrays.asList(
@@ -31,31 +31,33 @@ public class EntityChildRecordsLoopTest extends BaseTest {
             VALUE_ITEM_FIELD1, VALUE_ITEM_FIELD2, VALUE_ITEM_FIELD3);
 
     @Test
-    public void testCreateRecord(){
+    public void testCreateRecordsChRecLoop(){
 
         double sumOfAmountFieldValues = Double.parseDouble(START_BALANCE_VALUE);
 
-        ChildRecordsLoopEditPage childRecordsLoopEditPage = new MainPage(getDriver())
-                .clickChildRecordsLoopMenu()
-                .clickNewButton()
-                .fillStartBalance(START_BALANCE_VALUE);
+        for (int i = 0; i < 3; i++) {
 
-        for (int i = 1; i <= AMOUNT_OF_ROWS_IN_TABLE; i++) {
-            childRecordsLoopEditPage
-                .clickAddRowBtn()
-                .fillAmount(i, String.valueOf(LIST_VALUES_AMOUNT_FIELD.get(i - 1)))
-                .fillItem(i, LIST_VALUES_ITEM_FIELD.get(i - 1));
-            sumOfAmountFieldValues = sumOfAmountFieldValues + LIST_VALUES_AMOUNT_FIELD.get(i - 1);
+            sumOfAmountFieldValues = sumOfAmountFieldValues + LIST_VALUES_AMOUNT_FIELD.get(i);
         }
 
-        List<String> expectedRecord = Arrays.asList(
+        final List<String> expectedRecord = Arrays.asList(
                 String.format(Locale.ROOT,"%.2f", Double.parseDouble(START_BALANCE_VALUE)),
                 String.format(Locale.ROOT,"%.2f", sumOfAmountFieldValues));
 
-        childRecordsLoopEditPage.clickSave();
-
         ChildRecordsLoopPage childRecordsLoopPage = new MainPage(getDriver())
                 .clickChildRecordsLoopMenu()
+                .clickNewButton()
+                .fillStartBalance(START_BALANCE_VALUE)
+                .clickAddRowBtn()
+                .fillAmount(1, String.valueOf(VALUE_AMOUNT_FIELD1))
+                .fillItem(1, VALUE_ITEM_FIELD1)
+                .clickAddRowBtn()
+                .fillAmount(2, String.valueOf(VALUE_AMOUNT_FIELD2))
+                .fillItem(2, VALUE_ITEM_FIELD2)
+                .clickAddRowBtn()
+                .fillAmount(3, String.valueOf(VALUE_AMOUNT_FIELD3))
+                .fillItem(3, VALUE_ITEM_FIELD3)
+                .clickSave()
                 .getTextPaginationInfo(PAGINATION_INFO_STR_1_OF_1);
 
         Assert.assertEquals(childRecordsLoopPage.getRow(0), expectedRecord);
